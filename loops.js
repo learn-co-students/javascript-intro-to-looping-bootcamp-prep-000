ddefb9a6b5d0a5be2fd1e8462e64b58423987201
Katy <komal.vadukul@gmail.com>
2017-10-28 05:22:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+var array = [];
+
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+        array.push("I am a strange loop");
+      } else {
+        array.push(`"I am ${i} strange loop"`);
+      }
+    }
+    return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log ('done');
+    --n;
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
