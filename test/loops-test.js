440c595ea4b82194d54f63e63e3e4b7b55e6af46
Brandon Houghton <flatironschool@brandonhoughton.net>
2017-10-31 15:29:00 +0000

    Done.

--- test/loops-test.js
+++ test/loops-test.js
@@ -27,7 +27,7 @@ describe('loops', () => {
   })
 
   describe('forLoop(array)', () => {
-    it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
+    it('adds `"I am ${i} strange loop${i === 1 ? \'\' : \'s\'}."` to an array 25 times', () => {
       const [array, t] = makeArray()
       const strangeArray = forLoop(array)
 
@@ -48,7 +48,7 @@ describe('loops', () => {
       const n = Math.floor(Math.random() * 100)
 
       expect(whileLoop(n)).to.equal('done')
-      expect(spy).to.have.been.called.exactly(n)
+      expect(spy).to.have.been.called.exactly(n + 1)
 
       console.log.reset()
     })
