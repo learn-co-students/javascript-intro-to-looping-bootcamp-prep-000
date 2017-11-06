464df2b68bace5f61708b8c061dc0e107a1f9d20
Morganne Gagne <morgannegagne@gmail.com>
2017-11-06 16:50:10 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(array){
+  for (var i =0; i < 25; i++){
+    array.push(`I am ${i} strange loop`)
+  }
+  return array
+}
+
+function whileLoop(num){
+  while(num > 0){
+    console.log(num--)
+  }
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop()
+  }
+  while (maybeTrue() || array.length < 1)
+  return array
+}
