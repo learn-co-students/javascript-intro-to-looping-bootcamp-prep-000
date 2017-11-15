36a678841fd0a1ea2c857eb6e8ed9e96135da856
Bushra Taimoor <bushra376@hotmail.com>
2017-11-15 21:46:34 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+
+function forLoop(arr){
+  for(let i=0; i<25; i++){
+    if (i === 1){
+      arr.push ("I am 1 strange loop.");
+    }
+    else{
+      arr.push (`I am ${i} strange loops.`);
+    }
+  }
+  return arr;
+}
+function whileLoop(num){
+  while(num > 0){
+    console.log(--num);
+  }
+  return "done";
+}
+
+function maybeTrue(){
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  } while(maybeTrue() && (array.length>0))
+  return array;
+}
