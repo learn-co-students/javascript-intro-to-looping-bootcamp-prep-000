9bf058d34e35e5429e789d877328fb0003368750
Jay G <playerjayho@gmail.com>
2017-11-17 16:35:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
     if(i === 1){
     console.log("I am a 1 strange loop");
   } else {
-    console.log("I am a ${i} strange loops");
+    console.log(`I am a ${i} strange loops`);
     }
   }
 }
