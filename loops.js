d5d0834f87f87609f577ef9d5d5bcd89fb0d3848
Jay G <playerjayho@gmail.com>
2017-11-17 16:03:39 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+  for(let i=1; i <25; i++){
+    if(i === 1){
+    console.log("I am a 1 strange loop");
+  } else {
+    console.log("I am a ${i} strange loops");
+    }
+  }
+}
+
+function whileLoop(n) {
+    while() {
+
+    }
+}
+
+function doWhileLoop(array) {
+
+}
