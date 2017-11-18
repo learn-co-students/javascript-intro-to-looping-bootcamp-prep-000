addc9a0de033bddca38ad2433e74d922fad894b9
Theresa <theresa.l.morelli@gmail.com>
2017-11-18 03:27:43 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    var sentence = `I am ${i} strange loop`;
+    if (i !== 1) {
+      sentence += 's';
+    }
+    array.push(sentence);
+  }
+  return array;
+}
+
+function whileLoop(num) {
+  while (num - 1 >= 0) {
+    console.log(--num);
+  }
+  return 'done';
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+function doWhileLoop(array) {
+  do {
+    array.splice(array.length - 1);
+  } while (maybeTrue());
+  return array;
+}
