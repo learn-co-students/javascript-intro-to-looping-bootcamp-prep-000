4cee6e7bcbd7e79af9c11f3cd0d85dd57c2b5148
Angela <wang.angela90@gmail.com>
2017-11-21 00:21:59 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,14 @@
-function forLoop (array){
+function forLoop(array){
   for (var i = 0; i < 25; i++){
-  array.push(`I am ${i} strange loops.`);
-  console.log(array);
+    array.push(`I am ${i} strange loops.`);
+  }
+  return (array); //this makes sure that it doesn't repeate it 25 times
 }
+
 function whileLoop(){
-  
+  var timer = 50
+    while (timer > 0 ){
+  console.log (timer);
+  }
+  return (timer);
 }
