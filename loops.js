0b0c66911dc94b5e42849d1a92ea7e537e11e7ab
Angela <wang.angela90@gmail.com>
2017-11-22 01:11:22 +0000

    Done.

--- loops.js
+++ loops.js
@@ -5,10 +5,21 @@ function forLoop(array){
   return (array); //this makes sure that it doesn't repeate it 25 times
 }
 
-function whileLoop(){
-  var timer = 50
+function whileLoop(timer){
     while (timer > 0 ){
-  console.log (timer);
+  console.log (--timer); //decrement timer by 1; evaluates timer and then decrements it
   }
-  return (timer);
+  return("done");
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop (array){
+  do {
+    array.pop() // remove elemnts from the array
+  }
+  while (array.length>0); //while the array is more than 0, it will stop at 0
+  return array;
 }
