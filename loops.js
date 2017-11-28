4f43a1189489aaf8129958f8bb53c9db612ab5b8
Ryan Shur <ryanshur32@yahoo.com>
2017-11-28 00:58:30 +0000

    Done.

--- loops.js
+++ loops.js
@@ -26,7 +26,8 @@ function doWhileLoop(array){
     return Math.random() >= 0.5
   }
     do {
-      array.pop()
+       array.pop()
     } while(array.length > 0 && maybeTrue()){
     }
+    return array
 }
