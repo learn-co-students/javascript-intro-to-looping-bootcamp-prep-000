705c6bfa3dcbbb9226df32981e5fd0fde1751cbb
Ryan Shur <ryanshur32@yahoo.com>
2017-11-28 00:37:24 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,32 @@
 function forLoop(array){
-
     for(let i = 0; i < 25; i++ ){
-
       if (i === 1 ) {
-        console.log('I am 1 strange loop');
+        array.push('I am 1 strange loop');
       } else {
-        console.log("i am" + i +'strange loops');
+        array.push("i am " + i +' strange loops');
       }
+    }
+    return array
+}
+
+function whileLoop(n) {
+  while(n >= 0) {
+    console.log(n)
+    n--;
+      if(n === 0) {
+      return 'done'
+    }
+  }
+}
+
+
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+    do {
+      array.pop()
+    } while(array.length > 0 && maybeTrue()){
     }
 }
