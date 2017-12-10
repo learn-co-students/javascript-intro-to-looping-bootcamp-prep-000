9a18a4aed960631b6c7d4d752263f5a39a722e81
Siyu Zheng <siyu.zheng1@gmail.com>
2017-12-10 23:24:34 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(myArray){
+  for (let i=0; i<25; i++){
+    if (i==1){
+     myArray.push("I am 1 strange loop.") 
+    } else {
+      myArray.push("I am ${i} strange loops.")
+    }
+  }
+  return myArray
+}
+
+function whileLoop(myNumber){
+  while (myNumber > 0){
+    console.log(--myNumber)
+  }
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(myArray){
+  do {
+    myArray.pop()
+  } while (maybeTrue() && myArray.length > 0)
+  
+  return myArray
+}
