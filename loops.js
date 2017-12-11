d67400db697eca53fe7b9aa425dabbfd5c8c7d8b
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-12-11 03:58:02 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,6 @@
-var array= []
-
-function forLoop () {
-for([let i=0; i<25; i++]){
-console.log('I am ${i} strange loop')
+function forLoop (array) {
+  for(var i=0; i<25; i++){
+    array.push('I am ${i} strange loops.')
+  }
+  return array
 }
-}
-return array
