ebd23d4aa64215d82d33d3c2e93db6f31492e496
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-12-13 03:05:14 +0000

    Done.

--- loops.js
+++ loops.js
@@ -4,3 +4,27 @@ function forLoop (array) {
   }
   return array
 }
+
+
+function whileLoop (number){
+  while (number>0){
+    console.log(--number)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) { 
+  function maybeTrue() { 
+     return Math.random() >= 0.5   }  
+ do { 
+   array = array.slice(1) 
+ } while (array.length > 0 && maybeTrue())  
+  return array 
+}
+
+// function doWhileLoop (array){
+//   do {remove elements from the
+//     array until the array is empty}
+//     while(array.length > 0 && maybeTrue())
+//   return array
+// }
