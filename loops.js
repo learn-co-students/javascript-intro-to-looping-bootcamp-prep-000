75aaa772f6344aa98a0adf9652ec69e9675ee2b3
Van A. Dao <daoanhvan81@gmail.com>
2017-12-18 02:45:50 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,21 +2,24 @@ function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
+  return array;
 }
 
 function whileLoop(n) {
   while (n > 0) {
     n--;
+    console.log('Counting down ' + n);
   }
-  console.log('done');
+  return ('done');
 }
 
 function maybeTrue() {
-  return Math.randome() >= 0.5;
+  return Math.random() >= 0.5;
 }
 
 function doWhileLoop(array) {
  do {
    array.pop();
- } while ((array.length > 0) || (maybeTrue()) === false));
+ } while ((array.length > 0) || (maybeTrue() === false));
+ return array;
 }
