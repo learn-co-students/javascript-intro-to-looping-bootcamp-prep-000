ee507428ac7ac8122bbbaae829de57949f6310b8
Jeremy Gottfried <jeremyagottfried@gmail.com>
2017-12-19 01:10:30 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,8 @@
+function forLoop(array) {
+ for (var i = 0; i < 25; i++) {
+    if (i === 1) { array.push("I am 1 strange loop.")
+  } else 
+  { array.push("I am ${i} strange loops.")
+}}  return array
+}
+
