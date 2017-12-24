e2123202e65d4c9136ee15ae06be1a0e974860c2
Matt Redmon <matt.redmon@live.com>
2017-12-24 02:32:58 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,35 @@
+
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(num)
+    num--
+  }
+  return "done"
+}
+
+
+
+function maybeTrue() {
+  return Math.random() >= .5 
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop() 
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
+
+
