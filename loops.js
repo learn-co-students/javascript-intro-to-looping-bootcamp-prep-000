37748cdf05ca92610df625629772d07ed445c149
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 02:06:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -6,7 +6,20 @@ for (var i = 0; i < 25; i++)
   array.push("I am " + parseInt(i+1) + " strange loops.")
   return array
   }
+
+function whileLoop(n){
 let countdown = n 
 while (countdown > 0) {
   console.log(--countdown)
+  }
+ return "done"
+}
+function maybeTrue(){
+  return Math.random() >= 0.5
+}
+function doWhileLoop(array){
+  do {
+    array.slice(0, array.length - 1)
+  } while (array.length > 0 && maybeTrue())
+  return array && "false"
 }
