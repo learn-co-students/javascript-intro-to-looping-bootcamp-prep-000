68f81397f792006e95ede6ed202e27f5af550423
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 01:29:04 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+for (var i = 0; i < 25; i++) 
+  if (i===0) 
+  array.push("I am 1 strange loop.")
+  else
+  array.push("I am " + parseInt(i+1) + " strange loops.")
+  return array
+  }
+let countdown = n 
+while (countdown > 0) {
+  console.log(--countdown)
+}
