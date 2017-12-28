22ab7c74f1c2ffa3b9edad4b4928e127abfe4590
David McGrann <dpmcgrann@gmail.com>
2017-12-28 00:07:56 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,13 @@
+function whileLoop(n) {
+  let countdown = n
+  while (countdown > 0) {
+    console.log(--countdown)
+  } return ('done')
+}
+
+function forLoop(array) {
+  var array = []
+  for (let i = 0; i < 25; i++) {
+    return ("I am ${i} strange loop${i === 0 ? '' : 's'}.")
+  }
+}
