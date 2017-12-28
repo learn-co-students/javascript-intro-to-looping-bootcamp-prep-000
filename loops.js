689c5043109c9d05405c0952587b0e82d59f1959
Felice Forby <feliceforby@gmail.com>
2017-12-28 13:08:36 +0000

    Done.

--- loops.js
+++ loops.js
@@ -27,7 +27,7 @@ function doWhileLoop (array) {
   
   do {
     array.pop();
-  } while (array.length > 0 || !maybeTrue() );
+  } while (array.length > 0 || !maybeTrue() ); //could use && maybeTrue(), as well
   
   return array;
 }
