6a8032241ab5c668fd96b6ca7f0ee7e0ead8878b
David McGrann <dpmcgrann@gmail.com>
2017-12-29 21:49:43 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,3 +1,13 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push("I am " + i + " strange loops.")
+    }
+  } return array
+}
+
 function whileLoop(n) {
   let countdown = n
   while (countdown > 0) {
@@ -5,9 +15,12 @@ function whileLoop(n) {
   } return ('done')
 }
 
-function forLoop(array) {
-  var array = []
-  for (let i = 0; i < 25; i++) {
-    return ("I am ${i} strange loop${i === 0 ? '' : 's'}.")
+function doWhileLoop(array) {
+  function maybeTrue() {
+   return Math.random() >= 0.5
   }
+  do {
+   array.pop();
+ } while (array.length > -1 && maybeTrue());
+    return array;
 }
