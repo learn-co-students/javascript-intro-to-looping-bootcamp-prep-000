ab87c7f21fc50479f494007973f329555c243856
Jennifer Argote <jenniarg21@gmail.com>
2018-01-10 05:13:30 +0000

    Fix functions

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(arr) {
   for ($i = 0; $i < 25; $i++) {
     if ($i == 1) {
-      console.log("I am one strange loop.");
+      arr[$i] = "I am one strange loop.";
     }
     else {
-      console.log("I am ${i} strange loops.");
+      arr[$i] = "I am " + $i + " strange loops.";
     }
   }
   return arr;
@@ -24,7 +24,7 @@ function maybeTrue() {
 
 function doWhileLoop(arr) {
   do {
-    array.shift();
-  } while (array.length > 0 && maybeTrue());
+    arr.shift();
+  } while (arr.length > 0 && maybeTrue());
   return arr;
 }
