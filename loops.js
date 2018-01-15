7032d218913c72a6605897a8adf887e2c4965074
kendocode <git@kendocoding.com>
2018-01-14 21:05:44 -0600

    Done

--- loops.js
+++ loops.js
@@ -12,3 +12,26 @@ function forLoop(array) {
 }
 
 
+function whileLoop(n) {
+  if (n < 0) {
+    return "Your number must be more than 0, schmuck."
+  }
+
+  while (n > 0) {
+    console.log(--n)
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop(arr) {
+
+  do {
+    arr.shift();
+  } while (arr.length > 0 && maybeTrue());
+  
+  return arr;
+}
+
