aa133c57410da21adcb1b4086bcf2cf2fd180ad7
butch cosio <butchcosio@yahoo.com>
2018-01-19 18:32:04 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,30 @@
-function forLoop (array){
-  for (let i=0; i < 25; i++) {
-    if (i=1) {
-    array.push ("I am ${i} strange loop")}
-      
-    else {array.push ("I am ${i} strange loop")}
-  }}
+function forLoop(stringArray){
+   for(var i=0; i<25; i++){
+     if (i==1) {stringArray.push (`I am 1 strange loop.`)}
+     else {stringArray.push (`I am ${i} strange loops.`)}
+   }
+   return stringArray
+}
+   
+function whileLoop(n){
+  while(n>0){
+    console.log("The number is : "+ n)
+    n--
+  }
+  if (n===0){
+    flag="done";
+  }
+  return flag;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(stringArray){
+  var newArray = stringArray.slice()
+  do {console.log(newArray.pop());
+  }
+  while(newArray.length > 0 && maybeTrue())
+  return newArray
+  }
