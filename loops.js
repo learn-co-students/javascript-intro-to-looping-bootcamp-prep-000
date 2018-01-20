a247cf0f0f0908545ef754145bc5dcbe31256662
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-20 22:36:20 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,42 @@
+function forLoop(array){
+  
+for(var i=0; i<=25;i++){
+  
+if (i===1){array.push("I am 1 strange loop")}
+else if (i>1){
+array.push(`"I am ${i}strange loops"`)}}
+
+return array}
+
+function whileLoop(n){
+  
+while(n>0){
+  
+console.log(n);}n--
+
+return "done"
+  
+}
+
+function whileLoop(n){
+  
+while(n>0){
+  
+console.log(n--);}
+
+if(n===0){return "done"}
+
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  
+do{array.pop()
+  
+} while(maybeTrue() && array.length>0)
+
+  return array
+}
