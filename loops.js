404eb61ff69475deb81fa2857de3a3cec354d6f2
Rosie Wang <rosiedwang@yahoo.com>
2018-01-25 21:52:35 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,36 @@
-function doWhileLoop(array)
+function forLoop(array)
 {
-    let i = 0;
-  do{
-    //var value=array.slice(-(i+1));
-  }while((maybeTrue())||(array.length>0));
+  for(var i = 0; i < 25; i++)
+  {
+    if(i===0)
+    {
+    array.push("I am 0 strange loops.");
+    }
+    else
+    {
+     array.push("I am ${i} strange loop.");
+    }
+  }
+  return array;
 }
+function whileLoop(number)
+{
+  let countdown = number;
+  while (countdown >0)
+  {
+    console.log(--countdown);
+  }
+  return 'done';
+}
+
 function maybeTrue() 
 {
-  return Math.random() >= 0.5; // Returns a random number between 0 
+  return Math.random() >= 0.5; // Returns a random number between
+} 
+function doWhileLoop(array)
+{
+  do{
+    array.pop();
+  }while(maybeTrue()||array.length>0);
+  return array;
 }
