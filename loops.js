74a7966c3eebb708a923984340194942209fdfd3
Darwin Smith <darwin911@gmail.com>
2018-01-25 18:35:06 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,37 @@
+// var darwinArray= ['bike', 'computer', 'pens']
 
 function forLoop(array) {
-  for (var i = 0, i <= 25, i++)
-  return `I am ${i} strange loops`
+  for (var i = 0; i < 25; i++) {
+      if (i === 1) {
+        array.push('I am 1 strange loop')
+      } else {
+        array.push(`I am ${i} strange loops`)
+      }
   }
+  return array
+}
+
+function whileLoop(num) {
+  let countdown = num;
+  while (countdown > 0){
+    console.log(countdown)
+    countdown--
+  }
+  return `done`
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  }
+  while (array.length > 0 || maybeTrue());
+  return array;
 }
