fcdd9dde9aa4b2cd718b90e48a58d9430733439c
Dori Levy <dori.levy@wework.com>
2018-01-30 17:43:48 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if ( i == 1 ) {
+      arr.push("I am 1 strange loop.")
+    } else {
+      arr.push("I am ${i} strange loops.")
+    }
+  }
+  return arr
+}
+
+function whileLoop(num) {
+  while (num !== 0 ) {
+    console.log(--num)
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(arr) {
+  do {
+    arr.pop()
+  } while (! maybeTrue() || arr.lenght === 0)
+  return arr
+}
