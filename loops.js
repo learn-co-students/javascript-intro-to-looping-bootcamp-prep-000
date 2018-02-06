af21a3ed23510973b72ed75bc9206805193070d4
Jyot Patel <jyot.95patel@gmail.com>
2018-02-06 17:01:18 +0000

    Done.

--- loops.js
+++ loops.js
@@ -6,4 +6,27 @@ function forLoop(array) {
       array.push(`I am ${i} strange loops.`)
     }
   }
+  
+  return array;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(number);
+    number--;
+  }
+  
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  
+  return array;
 }
