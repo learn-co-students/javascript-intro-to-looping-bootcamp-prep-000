29e748b369dbf8fd42f2ddd7d576c06ad42a08e1
Troy Lawrence <redirect@gmail.com>
2018-02-07 22:50:21 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,25 @@
 function forLoop (array) {
   for (let i=0; i<25; i++) {
-    if (i===1) {
-      return `"I am 1 strange loop!"`;
+    if (i===0) {
+      array.push(`"I am 1 strange loop!"`);
     } else {
-      return `"I am ${i} strange loops!"`;
+      array.push(`"I am ${i + 1} strange loops!"`);
     }
+    return forLoop();
   }
+}
+
+function whileLoop (n) {
+  while (n>0) {
+    console.log(n--);
+  }
+  return "done";
+}
+
+function doWhileLoop (array) {
+  do {
+    
+  } while (
+    
+  )
 }
