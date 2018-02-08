e523dc8a33eac3e4dbdf5999893d9875eff041e7
Joyce Chow <joycecyoj01@gmail.com>
2018-02-08 18:34:38 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(arr){
 
-  for(var i=1; i<=25; i++){
+  for(var i=0; i<25; i++){
     if(i === 1){
       arr.push("I am 1 strange loop.")
     }
@@ -25,7 +25,6 @@ function doWhileLoop(arr){
   function maybeTrue() {
     return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
   }
-
   do{arr.shift()
   }
   while(arr.length > 0 && maybeTrue()) {
