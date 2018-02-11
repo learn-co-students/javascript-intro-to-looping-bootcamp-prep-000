1f44213651909bd2e0c681cd48630656bb075ace
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-11 00:04:22 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 25; i++) {
+  consoe.log("I am" + i + "strange loop.")
+}
+
+let number = 100;
+
+while (number > 10) {
+  console.log("done.")
+}
+
+fucntion mayBeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  console.log('bebo')
+} while (mayBeTrue());
