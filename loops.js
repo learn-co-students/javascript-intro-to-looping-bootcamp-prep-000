edd14b6ba71c1974ec86d8fbc724dd5893e70ad6
Brent Bauman <brentbauman86@gmail.com>
2018-02-21 00:06:37 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array){
+for (let i = 0; i < 25; i++){
+  array.push(`I am ${i} strange loops`)
+}
+return array
+};
+
+function whileLoop(4){
+  while (i > 4)
+  console.log(--countdown)
+  return (`done`)
+};
+
+function doWhileLoop(array){
+function maybeTrue()
+do (array.length > && maybeTrue())
+return (array)
+};
