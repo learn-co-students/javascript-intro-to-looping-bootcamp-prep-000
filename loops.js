dc86f8ac692c308472bc7f23c790e538f49fa111
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-23 22:46:45 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop (arr) {
+  for (var i=0; i<25; ++i) {
+    arr.push(`I am ${i} strange loops.`);
+  }
+  return arr;
+}
+
+function whileLoop (n) {
+  while (n > 0) {
+    console.log(`{n}`);
+    n--;
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(arr) {
+  do {
+    arr.pop();
+  } while ((arr.length >= 0) && maybeTrue())
+  return arr;
+}
