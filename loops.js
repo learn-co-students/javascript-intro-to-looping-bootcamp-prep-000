ee51d822431931b834f51eef06b13b9baec89d14
Cutina Woof <gretchenista@gmail.com>
2018-02-27 18:47:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,33 +1,32 @@
-/* function makeArray () {
-  const array = [];
-  const t = Math.floor(Math.random() * 10);
-  for (var i = 0; i < t; i++) {
-   {console.log ("I am a strange loop.");
-   return array(t);}
-  }
-} */
-
 function forLoop(array) {
-   for (let i = 0; i < 25; i++) {
-   if (i === 1) {array.push ("I am ${i} strange loop."); }
-    else {
-      array.push("I am ${i} strange loops.");
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
     }
   }
+
+  return array
 }
 
+
 function whileLoop(n) {
-  n = Math.floor(Math.random() * 100);
-  while (n > 0 ) {
-    console.log(--n);
+  while (n > 0) {
+    console.log(--n)
   }
-  console.log('done');
+
+  return 'done'
 }
 
 function doWhileLoop(array) {
-do {
-  array.pop();
-} while (array.length > 0 && maybeTrue());
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
 
-return array; 
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
 }
