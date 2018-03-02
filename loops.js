9363634bf37bc742cdf02aeef1ead4b396b50c7d
Sonia  D. Nicas <sduarte22@gmail.com>
2018-03-02 19:06:06 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array){
+  for (var i = 0; i< 25; i++){
+    if(i===0){
+      array.push("I am 1 strange loop");
+    }
+    else {
+     array.push( "I am ${i} strange loops");
+    }
+  }
+  return array ;
+}
+
+function whileLoop(number){
+  while(number>0){
+    console.log(done);
+  }
+}
