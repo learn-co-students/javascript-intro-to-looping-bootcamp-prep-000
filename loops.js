3d07b9ccbeafb8f8d08d22f172743447aff9fdef
Brian Feibus <brianfeibus@gmail.com>
2018-03-07 22:57:56 +0000

    Done.

--- loops.js
+++ loops.js
@@ -22,8 +22,8 @@ function whileLoop (number){
 }
 
 function doWhileLoop(array) {
-do { array.pop();
-} while
-(array.length > 0 && maybeTrue());
-return array
-}
+  do { array.pop();
+    } while
+      (array.length > 0 && maybeTrue());
+          return array
+            }
