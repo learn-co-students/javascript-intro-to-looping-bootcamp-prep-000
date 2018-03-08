5bdb5e6b661ca5c81ba1984b582ed65ee84838d0
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-08 01:28:23 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,29 +1,23 @@
 function forLoop(arg){
-  for (var i = 1; i < 25; i++){
-    if (i === 1){
-    console.log(`I am a ${i} strange loop`);
-    }
-    if (i > 1){
-    console.log(`I am a ${i}  strange loops.`);
-  }
-}}
+  for (var i = 0; i < 25; i++){
+    arg.push(`I am ${i} strange loop ${i === 0 ?'':'s'}.`);
+  }return arg
+}
 function whileLoop(num){
   while(num > 0){
     console.log(num)
     num--
-  } console.log("done")
-}
-function doWhileLoop(arg){
- console.log("idk wtf should go here")
-  while(arg.length > 0);{
-    delete arg[arg.length - 1]
-  }
+  } return "done"
 }
 
 function maybeTrue() {
   return Math.random() >= 0.5 
 }
-while (maybeTrue()) {
-  console.log("And I ran; I ran so far away!");
+
+function doWhileLoop(array){
+ do {console.log("idk wtf should go here");}
+  while(maybeTrue() && array.length > 0);{
+     array.length = array.length - 1
+  }return array
 }
 
