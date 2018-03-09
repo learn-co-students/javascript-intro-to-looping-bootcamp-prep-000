10bacfb5262e8326ac04073e67512acf03b3323a
Jamie Catania <catania.jamie@gmail.com>
2018-03-09 05:04:45 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop (arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push("I am 1 strange loop.");
+    } else {
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+  return arr;
+}
+
+function whileLoop (num) {
+  while(num > 0) {
+    console.log(num);
+    num--;
+  }
+  return 'done';
+}
+
+function doWhileLoop (arr) {
+  do {
+    arr.shift();
+  } while(arr.length > 0 && maybeTrue())
+  
+  return arr;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
