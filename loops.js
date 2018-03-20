82e696fc818e81763144ea40f8a930886e111bd7
vincent <omegaortiz1997@gmail.com>
2018-03-20 18:00:26 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++)
+  if (i === 1) {
+    console.log("I am 1 strange loop");
+  } else (i > 1); {
+    console.log("I am ${i} strange loops");
+  }
+  return array;
+}
