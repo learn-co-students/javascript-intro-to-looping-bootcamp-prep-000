b4043fabe7f1ff9051b310b583fdc8ac361bae80
Christopher <christopherccm@msn.com>
2018-03-22 20:56:27 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,28 +1,11 @@
 function forLoop(array) {
-  var i
+  var i = 0;
   for (let i = 0; i < 25; i++) {
-    if (i = 1) {
-    array.push("I am 1 strange loop.");
+    if (i === 1) {
+      array.push("I am 1 strange loops.");
     } else {
-      array.push("I am ${i} strange loops.");
+      array.push("I am ${i} strange loops.")
     }
   }
+  return array;
 }
-
-//function whileLoop(number) {
-//  return
-//  let countdown = number; number - 1;
-//  while (countdown > 0) {
-//    console.log(--countdown)
-//  }
-//}
-
-//function doWhileLoop(array) {
-//  function maybeTrue() {
-//    return Math.random() >=0.5
-//  }
-//  do {
-//    array.shift()
-//  } while array.length > 0 && maybeTrue();
-//  return array;
-//}
