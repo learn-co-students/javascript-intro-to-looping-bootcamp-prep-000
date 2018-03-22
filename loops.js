0ba8c90781147c12e249b66ea40f1c96df0974bc
Adam Cohn <adamcohn88@gmail.com>
2018-03-22 15:02:40 -0400

    Done.

--- loops.js
+++ loops.js
@@ -1,28 +1,31 @@
-function forLoop (array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push(`I am 1 strange loop.`);
+function forLoop(array) {
+  for (var i = 0; i<25; i++) {
+    if (i == 1) {
+      array.push(`I am 1 strange loop`)
     } else {
-     array.push(`I am ${i} strange loops.`);
-  }
-  }
-return array;
+    array.push(`I am ${i} strange loops.`)
+    };
+  };
+  return array;
 }
 
-function whileLoop(countdown) {
-  while (countdown > 0) {
-  console.log(--countdown)
+function whileLoop(n) {
+  while (n > 0) {
+        console.log(`${n}`);
+        n--;
+    }
+    if (n == 0) {
+      return 'done';
+    };
   }
-    return 'done';
-}
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-function doWhileLoop(array) {
-do {
-  array.pop();
-} 
-while (array.length > 0 && maybeTrue());
-return array;
-}
+  function maybeTrue() {
+    return Math.random() >= 0.5 
+  }
+
+  function doWhileLoop(array) {
+    do {
+      array.pop();
+      return array;
+    } while (maybeTrue());
+  }
