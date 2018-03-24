8de1f006b74cc43a03fb785985e3abf7028bc93d
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-24 18:47:09 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array){
+  for (let i = 0; i<25; i++) {
+    if (i<1) {
+      return (`I am 1 strange loop`)
+    }
+  else if(i>2){
+    return (`I am ${i} strange loops`)
+    return array
+  }
+  }
+}
+
+
+
+function whileLoop(n){
+  return Math.random()>0
+}
+
+while (whileLoop(n)){
+  return('done')
+}
+
+
+function doWhileLoop(array){
+return Math.random()
+}
+
+do{
+  return('done');
+} while (array.length > 0 && doWhileLoop());
