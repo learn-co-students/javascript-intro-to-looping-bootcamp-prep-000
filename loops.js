ba817e1da827ee6406f6abdc22b45706d705fb7f
vincent <omegaortiz1997@gmail.com>
2018-03-25 18:05:19 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,24 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++)
+  for (let i = 0; i < 24; i++) {
   if (i === 1) {
-    console.log("I am 1 strange loop");
+    array.push("I am 1 strange loop.");
   } else (i > 1); {
-    console.log("I am ${i} strange loops");
+    array.push("I am ${i} strange loops");
   }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+     console.log(--n);
+}
+  return ("done");
+}
+
+function doWhileLoop(array) {
+  do {
+    console.log(--array.length);
+  } while (array.length>0);
   return array;
 }
