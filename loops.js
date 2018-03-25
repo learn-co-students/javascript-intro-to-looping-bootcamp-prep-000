9d8be998c88397821e51289a5e48502d170f8357
Alex Edwards <info@acedigitalagency.net>
2018-03-25 21:11:31 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,20 @@
-
-
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1) { array.push("I am " + i + " strange loop.") };
-    else { array.push(`I am ${i} strange loops.`) };
+      if (i === 1) {
+        array.push("I am 1 strange loop.")
+      }
+      else {
+        array.push(`I am ${i} strange loops.`)
+      }
+  }
+  return array;
 }
-return array;
+
+let n = 10;
+
+function whileLoop(n) {
+  while ( n > 0 ) {
+      console.log(--n)
+    }
+   return 'done';
 }
