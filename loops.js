b6715279e83b888e2975d3fa659e42265f534707
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 16:15:44 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,48 @@
+ function whileLoop (num){
+     while (num > 0) {
+       console.log(num = num - 1);
+         if (num === 0)
+       return "done"
+  }
+}
+whileLoop (45)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+function doWhileLoop(arr){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+  do {
+    arr.pop()
+  } while ((arr.leth==20) || !maybeTrue() )
+
+return arr
+}
+
+
+
+ var array= [];
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+      if (i === 1) {
+        array.push("I am 1 strange loop.")
+      }
+      else {
+        array.push(`I am ${i} strange loops.`)
+      }
+  }
+  return array;
+ 
+}
+
+
+
+
+
+
