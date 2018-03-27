d52a8de933e26df2fd081ee7df956decc8cf8b28
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 19:40:29 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(arr){
+  for (i=0; i<25; i++){
+    if (i===1){
+      arr.push("I am 1 strange loop.")
+    }
+    else {
+      arr.push(`I am ${i} strange loops.`)
+    }
+  }return arr
+}
+function whileLoop(num){
+  while (num>=0){
+    console.log(--num)
+    if (num === 0){
+      return "done"
+    }
+  }
+}
+function doWhileLoop(arr){
+
+
+do {
+  arr.pop()
+} while (arr.length > 0 && maybeTrue());
+function maybeTrue() {
+return Math.random() >= 0.5
+}
+return arr
+}
