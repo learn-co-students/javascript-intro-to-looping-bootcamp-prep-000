ca5d878d5a9e5b9af2542f796893b2d20c57c924
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-28 16:42:21 +0000

    Done.

--- test/loops-test.js
+++ test/loops-test.js
@@ -15,7 +15,7 @@ function makeArray() {
   const t = Math.floor(Math.random() * 10)
 
   for (let i = 0; i < t; i++) {
-    array.push("I am a strange loop.")
+    array.push("I am 1 strange loop.")
   }
 
   return [array, t]
