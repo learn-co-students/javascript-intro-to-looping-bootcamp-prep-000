96c6586a3fbc2efdbd0128138df8a8e7950b5e11
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-29 01:21:50 +0000

    Done.

--- loops.js
+++ loops.js
@@ -7,3 +7,22 @@ function forLoop(array) {
   }
   return array
 }
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+function doWhileLoop(array) {
+  do {
+    return array.slice(0, array.length - 1)
+  } while (array.length > 0 && maybeTrue())
+}
+  
