3be8b4d4920647054d20cfcb1f46ed07cf5107dc
Jackie First <jackiejeep@gmail.com>
2018-03-31 23:19:53 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,26 +1,28 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push("I am a strange loop.")
+      array.push("I am ${i} strange loop.")
     } else {
       array.push("I am ${i} strange loops.")
     }
   }
   return array;
 }
+
 function whileLoop(n) {
-  let countdown = n;
-  while (countdown > 0); {
-    console.log(--countdown);
+  while (n > 0); {
+    console.log(n--);
   }
   return "done";
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5}
+  
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
   do {
     array.pop();
-  } while (array.length > -1 && maybeTrue());
+  } while (array.length > 0 && maybeTrue());
   return array;
 }
+
