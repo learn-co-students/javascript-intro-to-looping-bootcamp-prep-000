28074437c6200689c446c43c24704a5c5637ead9
Rommy Elyan <relyan@email.arizona.edu>
2018-04-03 20:54:47 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+var array = {};
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    if (i==1){
+      array.push('I am 1 strange loop.')
+    }else {
+    array.push(`I am ${i} strange loops`)    
+    }
+  }
+    console.log(array)
+    return array
+}
+
+function whileLoop(n){
+  let countdown = n
+  while (countdown>0){
+    console.log(--countdown);
+  }    
+  return 'done';
+}
+
+function doWhileLoop(array){
+  do {array.pop()}
+  while (array.length >0);
+  return array
+}
+  
