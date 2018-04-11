e166e743f86212f5166219bfb6e7e137d9f0fd42
Kevin Sudor <ksudor42@gmail.com>
2018-04-11 15:31:07 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+  for (let i = 1; i < 26; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push("I am " + i + " strange loops.")
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    n -= 1;
+    console.log(n);
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array) {
+  do {
+    array = array.slice(1);
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
