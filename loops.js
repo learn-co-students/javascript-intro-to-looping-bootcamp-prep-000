f075d46d2679e812958f124ff98d94e2f4faf6b1
David Femi Adeyemi <davidadeyemi@my.unt.edu>
2018-04-13 03:28:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,23 @@
-function doWhileLoop(arr){
-  do{
-    arr.pop();
+function forLoop(array){
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop ${i === 0 ? '' : 's'}.`)
   }
-  while (maybeTrue() || arr.length === 0);
+  return array;
+}
+
+function whileLoop(n){
+  while (n > 0) {
+    console.log(--n);
+  }
+  return "done";
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  }
+  while (maybeTrue() );
+  return array;
 }
 
 function maybeTrue() {
