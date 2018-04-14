5a54b906c47a7a4da7ffbc3c9861ec4ae81bc3e6
Wasif Khan <waasif.khaan@gmail.com>
2018-04-14 04:34:31 +0000

    Done.

--- loops.js
+++ loops.js
@@ -6,6 +6,7 @@ for (i=0;i<25;i++){
   return array
 }
 function whileLoop(n) {
+<<<<<<< HEAD
   while(n>0) {
     console.log(--n)};
   return "done"
@@ -17,4 +18,9 @@ function doWhileLoop(array){
   do {array.shift()}
 while(array.length > 0 && maybeTrue()> 0);
 return array
+=======
+  while(n>0)
+  {console.log(--n)};
+  console.log('done')
+>>>>>>> cbf0c3d8ce9323363c4d4a0763719c950e7e522d
 }
