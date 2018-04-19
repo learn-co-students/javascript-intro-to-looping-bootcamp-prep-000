74e6dcecf13e846b2ddb0913ddf1f06704657fd4
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-19 02:59:34 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,10 +3,27 @@ function forLoop(array) {
     if (i === 1) {
       array.push("I am 1 strange loop.")
     } else {
-      array.push('I am ${i} strange loops.');
+      array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
     }
   }  
   return array;
 }
 
-function whileLoop()
+function whileLoop(n) {
+  let countdown = n;
+  
+  while(countdown > 0) {
+    console.log(countdown--);
+  }
+  return "done";
+ }
+ 
+ function doWhileLoop(array) {
+   function maybeTrue() {
+     return Math.random() >= 0.5
+   }
+     do {
+       array.pop();
+     } while (array.length > -1 && maybeTrue());
+   return array;
+ }
