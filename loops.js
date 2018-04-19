30861e260df1c53ebccf81dd7acecaee1182b0e1
sherjil999@gmail.com <sherjil999@gmail.com>
2018-04-19 22:23:45 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for(i = 0; i < 25; i++) {
+      array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+    }
+  return array
+}
+
+function whileLoop(number) {
+  while(number > 0) {
+    console.log(number)
+    number--
+  }
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (maybeTrue()  && array.length > 0)
+  
+  return array
+}
