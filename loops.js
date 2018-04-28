9a468882f7900ef82ecfe332b56580fa6c8dd0fb
Sammy Akharaz <ketrixz@gmail.com>
2018-04-28 22:59:13 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(arr) {
     if (i === 1){
       arr.push("I am 1 strange loop.")
     }else {
-      arr.push("I am ${i} strange loops.")
+      arr.push('I am ${i} strange loops.')
     }
   }
   return arr
