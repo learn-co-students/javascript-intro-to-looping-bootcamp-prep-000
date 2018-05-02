8ec5c3071910e016154e21d63acac5082f0b315b
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 05:28:13 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,15 +1,14 @@
 function forLoop(array) {
-  for (var i = 1; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array[i] = "I am 1 strange loop.";
+      array.push(`I am 1 strange loop.`); //array[i] = "I am 1 strange loop.";
     } else {
-      array[i] = `I am ${i} strange loops.`;
+      array.push(`I am ${i} strange loops.`); //array[i] = `I am ${i} strange loops.`;
     }
   }
 return array;
 }
 
-  
 function whileLoop(number) {
   while (number > 0) {
     console.log(--number);
