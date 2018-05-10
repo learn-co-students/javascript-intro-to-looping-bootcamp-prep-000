eb22516cd876ee6daf18e4d1cae454fc9297d080
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-10 14:55:43 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,10 +3,10 @@ let i = 0
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     console.log("I am ${i} strange loops.")
-  }
   if ( i === 0 ) {
     console.log ("This isn't right.")
   } else if (i > 25) {
     console.log ("You've gone too far!")
+  }
   }
 }
