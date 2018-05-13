7da7340b3360b097a71c6f4e75610e2711b8a250
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-13 07:33:22 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,13 @@
+
 function forLoop(array){
   var i;
   for(let i=0; i<25; i++){
-    if (i === 1)
-      console.log("I am 1 strange loop.")
-    else 
-      console.log (`I am ${i} strange loops.`)
-    return array
+    if(i === 1){
+      array.push("I am 1 strange loop.");
     }
+    else{
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
 }
-
