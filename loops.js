66641e303fc696f03ca2af9468d89da6b49f98ce
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-22 14:03:20 +0000

    Done.

--- loops.js
+++ loops.js
@@ -18,3 +18,11 @@ function whileLoop(n){
   return "done"
 }
 
+function doWhileLoop(array){
+    function maybeTrue() {
+      return Math.random() >= 0.5
+  } do {
+   array.pop()
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
