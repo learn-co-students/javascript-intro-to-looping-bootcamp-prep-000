375447bbe51d9389a8d2590f71a08170ffd49468
Jess Jacobson <jessanova@gmail.com>
2018-05-22 20:22:03 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+var array = []
+function maybeTrue(){
+  return Math.random() >= 0.5
+}
+
+function forLoop(array){
+  for(var i=0;i<25;i++){
+    if(i==1){
+      array.push("I am 1 strange loop")
+    }else{
+      array.push("I am ${i} strange loops")
+    }
+  }
+  return array
+}
+
+function whileLoop(x){
+  while(x>0){
+    console.log("x")
+    x--
+  }
+  return "done"
+}
+
+function doWhileLoop(array){
+  do{
+    array.length--
+  }while(array.length > 0 && maybeTrue())
+  return array
+}
