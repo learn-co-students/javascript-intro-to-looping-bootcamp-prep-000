fd0268e238847417f9e928cd1645c57e83875e7e
Victor Monterroso <vicmmonterroso@gmail.com>
2018-05-22 15:56:28 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (var index = 0; index < array.length; index++) {
+  for (var index = 0; index < 25; index++) {
     if(index === 1) {
       array.push("I am 1 strange loop.");
     } else {
@@ -11,21 +11,21 @@ function forLoop(array) {
 }
 
 function whileLoop(number) {
-  while(number >= 0) {
+  while(number > 0) {
     console.log(number);
     number--;
   }
   return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
   do {
     array.pop();
-  } while (maybeTrue() && array.length > 0)
+  } while (maybeTrue() && array.length > 0);
   
   return array;
-}
-
-function maybeTrue() {
-  return Math.random() >= 0.5
 }
