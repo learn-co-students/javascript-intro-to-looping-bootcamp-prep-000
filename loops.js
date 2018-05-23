6ff04d95b28e301ca6c0d8fb402b1a23fb368fb9
Kathy <kleboeuf@townisp.com>
2018-05-23 21:08:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-
+/*
 function forLoop(array) {
   
 	//let newArray = [];
@@ -14,7 +14,18 @@ function forLoop(array) {
 	return array;
    }
   //console.log(forLoop());
+*/
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
 
+  return array
+}
 /* ===================================  */
 
 function whileLoop(number) {
