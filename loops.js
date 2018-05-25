9fa69fb4773c8782e568bf76ca63f98f6d3f0493
Chris Gioeli <cgioeli@gmail.com>
2018-05-25 00:39:48 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function whileLoop(n) {
+ while (n > 0) {
+    console.log(n);
+    n = --n;
+  }
+  return "done";
+}
+
+
+
+
+function doWhileLoop() {
+ return ();
+}
+do {
+  console.log ('doo-bee-doo-bee-doo');
+} while (array.length > 0 && maybeTrue());
+
