e53165b5705e857f940dad0b75471fd44a57580d
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-31 07:38:47 +0000

    Done.

--- loops.js
+++ loops.js
@@ -25,3 +25,17 @@ function whileLoop(number) {
   return "done"
   
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+  }
+ 
+do {
+  array.pop()
+} 
+while (array.length > 0 && maybeTrue());
+  
+  return array
+  
+}
