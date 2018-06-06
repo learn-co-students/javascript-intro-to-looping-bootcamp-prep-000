545071c411966c50a65541ecfa8a5d06401e7d07
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-06 13:38:39 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 // adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times:
 function forLoop(array)
 {
-  for(let i = 0; i < array.length + 25; i++)
+  for(let i = 0; i < 25; i++)
   {
     if(i === 1)
     {
