de94838d84fbfbfc3ab1b28d1db65a316864bd7a
Sarah Dilworth <sarah.petty18@gmail.com>
2018-06-08 17:01:17 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(){
+for (let i =0; i < 25; i++){
+if (i == 1){
+console.log("I am 1 strange loop.");
+}
+else {
+console.log(`I am ${i} strange loops.`);
+}
+}
+}
+
+function whileLoop(n){
+console.log(--countdown);
+return 'done';
+}
+
+function doWhileLoop(){
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+do {
+console.log  (array.length > 0 && maybeTrue());
+} while (maybeTrue());
+}
