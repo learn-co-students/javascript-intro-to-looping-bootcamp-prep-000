d373229210727aa5da57cbb794acb5dad0ac3bdf
patrick.shim91@yahoo.com <patrick.shim91@yahoo.com>
2018-06-13 17:43:26 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,13 +1,15 @@
 function forLoop(array) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
-       array.push(`I am 1 strange loop.`)
+       array.push("I am 1 strange loop.")
      } else {
-       array.push(`I am ${i} strange loops.`)
+       array.push("I am ${i} strange loops.")
      }
   }
   return array
 }
+
+
 
 function whileLoop(n) {
   let countdown = n;
@@ -15,6 +17,10 @@ function whileLoop(n) {
     {console.log(--countdown)}
     return 'done'
 }
+
+
+
+
 
 function doWhileLoop(array) {
   function maybeTrue() {
