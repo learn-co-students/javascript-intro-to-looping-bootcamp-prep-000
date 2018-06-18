61d6015733a1683c6b4770acb0b313a652ae4398
karen wang <karenkarenmio@gmail.com>
2018-06-18 21:56:07 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,32 @@
-const array=[]
+const array = [];
 
-for (var i=0; i<25; i++){
-  if (i=1){
+function forLoop(array) {
+  let i=0;
+  for (i===0; i<25; i++){
+   if (i===1) {
     array.push("I am 1 stange loop.")
-  } else {
+   } else {
     array.push('I am ${i} strange loops.')
+   }
   }
+return array;
 }
+ 
+function whileLoop() {
+  let countdown=100;
+  while (countdown>0) {
+    console.log(--countdown)
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >=0.5
+}
+
+function doWhileLoop(array) {
+  do{array.splice(0,1)}
+  while (array.length>0 && maybeTrue());
+  return array;
+}
+
