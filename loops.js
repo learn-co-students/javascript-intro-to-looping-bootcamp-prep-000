dd8027e5bb4d9d46d71a917c982720f678595ec3
Denise Sison <tinyboxes@gmail.com>
2018-06-19 21:20:01 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+for ([initialization]; [condition]; [iteration]) {
+  [loopBody]
+}
+  
+for (let i = 0; i < 25; i++) {
+    array.push("I am a strange loop.")
+return [array, t]}
+  
+
+while () {}
