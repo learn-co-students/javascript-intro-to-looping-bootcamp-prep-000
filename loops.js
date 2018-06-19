225d58d8e6013c56ccbc9e21f460944018a86561
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 15:49:01 +0000

    Done.

--- loops.js
+++ loops.js
@@ -4,14 +4,17 @@ function forLoop(array)
 {
 for (let i = 0; i < 25; i++) 
 {
-  if (i = 1) {
+  if (i === 1) {
     array.push("I am 1 strange loop.")
-    } else if (i < 25) {
+  } else if (i<25)
+  {
     array.push("I am ${i} strange loops.")
-} else 
-
-  return array }
+} 
 }
+  return array 
+}
+
+parseInt('number', 10)
 
 function whileLoop(number) {
  
