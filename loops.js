26dec1e14681981c5a06e9c2d14afdbeb85da1d5
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 14:24:31 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,14 @@
+var array = []
+
+function forLoop(array)
+{
+for (let i = 0; i < 25; i++) 
+{
+  if (i = 1) {
+    array.push("I am 1 strange loop.")
+    } else if (i < 25) {
+    array.push("I am ${i} strange loops.")
+} else 
+
+  return array }
+}
