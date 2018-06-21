2b6dee0122ea4dacd61e0bd4f4e2a21606dc13f3
Beerscheba C <designergo@aol.com>
2018-06-21 16:23:52 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,30 +1,36 @@
+let n = 10;
 
-
-function forLoop(array){
- for ( let i = 0; i <25; i++);{
-   if (i === 1)
-   console.log("I am 1 strange loop.");
- 
-  else if(i=>25) 
-  console.log ("I am ${i} strange loops.");
-}
-}
-var n= 10;
 function whileLoop(n){
-  
-  while(n<=0){
+while ( n > 0 ) {
     
-    console.log('done')
-  }
+    console.log(n--);
+    
+}
+return ('done');
+}
 
-  
-  
-  
-  
-  
-  function doWhileLoop(array){
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5 }
     
     
-  }
+
+  do {
+     array.pop()
+  } while ( array.length > 0 && maybeTrue())
   
+return array
+}
+function forLoop(array){
+  
+ for( let i = 0; i <25; i++) {
+   
+   if( i ==1){
+   
+     array.push('"I am 1 strange loop."')
+   } else {
+     array.push("I am ${i} strange loops.")
+   }
+ }
+return array
 }
