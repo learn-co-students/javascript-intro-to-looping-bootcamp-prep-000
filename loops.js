0421aace3bf6586c197f09cc5d2595c29914ab3c
Beerscheba C <designergo@aol.com>
2018-06-21 11:38:40 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+
+
+function forLoop(array){
+ for ( let i = 0; i <25; i++);{
+   if (i === 1)
+   console.log("I am 1 strange loop.");
+ 
+  else if(i=>25) 
+  console.log ("I am ${i} strange loops.");
+}
+}
+var n= 10;
+function whileLoop(n){
+  
+  while(n<=0){
+    
+    console.log('done')
+  }
+
+  
+  
+  
+  
+  
+  function doWhileLoop(array){
+    
+    
+  }
+  
+}
