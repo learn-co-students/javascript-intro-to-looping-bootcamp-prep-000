5d00aa5cb43f81848f260eb1f3014f8fa242cb8b
Daniel Johnson <danielfrankjohnson@gmail.com>
2018-06-25 23:13:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,14 @@
+function forLoop(array) {
 for (let i = 0; i < 25; i++) {
   if (i == 1) {
-    
-    console.log("I am 1 strange loop");
+  
+  array.push("I am 1 strange loop");
   } else {
-    console.log("I am ${i} strange loops");
+  array.push("I am ${i} strange loops");
+    
   }
+  
+}
+return array
+  
 }
