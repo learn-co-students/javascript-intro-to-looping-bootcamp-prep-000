f8a3cdc00df514ce16a58a0b7abe211bb5abca5b
Susann Weissova <suzi.weissova@gmail.com>
2018-06-25 08:46:56 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,27 @@
-function forLoop() {
-  for (var i = 1; i < 25; i++){
-    const array=[]
-    array.push("I am ${i} strange loop");
+function forLoop (array){
+  for(i=1; i<25; i++){
+    if (i===1){
+      array.push("I am 1 strange loop")
+    } else {
+      array.push("I am ${i} strange loops.")
+    }
   }
+  return array;
+}
+
+function whileLoop(num){
+  while (num>0){
+    console.log(--num)
+  }
+  return 'done'; 
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+  return array
 }
