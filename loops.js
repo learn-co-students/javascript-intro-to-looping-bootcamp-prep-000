8c4be542a4fd1084781d2b59a6797c2a6c492b59
jenniferlong8 <jlong8@bu.edu>
2018-07-02 15:14:06 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
-var array = [];
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push["I am 1 strange loop."]
+      array.push("I am 1 strange loop.")
     } else: {
-      array.push[`I am ${i} strange loops.`]
+      array.push(`I am ${i} strange loops.`)
     }
-    console.log(array);
+  return array
   }
 }
