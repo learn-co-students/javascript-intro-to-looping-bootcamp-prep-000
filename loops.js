753535d0b595a72863d666b41388bf045223bc67
jenniferlong8 <jlong8@bu.edu>
2018-07-02 15:59:34 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,32 @@
+// start counting from 0, add string to array 25 times
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.")
-    } else: {
+    } else {
       array.push(`I am ${i} strange loops.`)
     }
 
   }
+    return array
+}
+// count down from passed in number to 0
+function whileLoop(n) {
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+    return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.shift()
+  } while ((array.length > 0) && (maybeTrue()));
+
     return array
 }
