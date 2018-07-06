e71976e3dce7cb613e25d6e4e6b94771f627161b
Benjamin Lee <nycxbl@hotmail.com>
2018-07-06 20:30:17 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,29 @@
 function forLoop(array){
-  for (var i =0; i<25; i++)
-  {
+  for (let i =0; i<25; i++){
     if (i===1){
-    array[i] = "I am 1 strange loop.";
-    } else {
+      array[i] = "I am 1 strange loop.";
+    } 
+    else {
       array[i] = "I am ${i} strange loops.";
     }
   }
+  return array;
+}
+
+function whileLoop(number){
+  while (0<number){
+    console.log(--number);
+  }
+  console.log("done");
+}
+
+function doWhileLoop(array){
+  do {
+    console.log('doo-bee-doo-bee-doo')
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
 }
