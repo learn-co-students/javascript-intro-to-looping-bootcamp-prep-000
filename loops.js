74ef0727eeae440f9b3dd24b91be7ded08eb5395
Benjamin Lee <nycxbl@hotmail.com>
2018-07-06 20:12:15 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array){
+  for (var i =0; i<25; i++)
+  {
+    if (i===1){
+    array[i] = "I am 1 strange loop.";
+    } else {
+      array[i] = "I am ${i} strange loops.";
+    }
+  }
+}
