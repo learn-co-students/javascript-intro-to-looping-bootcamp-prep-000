024303645381f03f684669166216f056f23d642f
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 16:54:59 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,16 +1,17 @@
 function forLoop (array) {
-for (let i=0; i<25; i++ ){
-  if (i===1) {array.push(`I am 1 strange loop.`);
+for (let i=0; i<26; i++ ){
+  if (i===0) {array}
+  else if (i===1) {array.push(`I am ${i} strange loop.`);
     }
     else {array.push(`I am ${i} strange loops.`);
     }
 }
-  return array
+  return array;
 }
 
-let x=5;
+
 function whileLoop (x){
-  
-  while (x>0)
-  console.log ("done")
+  let countdown=x;
+  while (countdown>0){console.log(--countdown)}
 }
+return "done";
