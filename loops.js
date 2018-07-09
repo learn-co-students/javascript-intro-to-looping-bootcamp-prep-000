59f6767f5d323f01ccd2a4cf5376ca4802de1a0c
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 17:25:56 +0000

    Done.

--- loops.js
+++ loops.js
@@ -16,6 +16,9 @@ function whileLoop(x){
   return "done";
 }
 
-function doWhileLoop(array){
-  
+function maybeTrue() {
+  return Math.random() >= 0.5
 }
+function doWhileLoop(array){
+  do {array.pop()} 
+} while (array.length>0 && maybeTrue());
