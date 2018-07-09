291584a374763d5483aec01ea34aba24c9edc9cb
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-07-09 00:56:43 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,4 @@
-function makeArray() {
-  const array = []
-  const t = Math.floor(Math.random() * 10)
-
-  for (let i = 0; i < t; i++) {
-    array.push("I am a strange loop.")
-  }
-
-  return [array, t]
+array []
+for (var i = 0; i < array.length; i++) {
+  array.push(`I am ${i} strange loop.`)
 }
