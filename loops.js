ceb618b91d70d8e4bfec6d57133c4d0a78825240
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 16:05:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop (array) {
+for (let i=0; i<25; i++ ){
+  if (i===1) {array.push(`I am 1 strange loop.`);
+    }
+    else {array.push(`I am ${i} strange loops.`);
+    }
+}
+  return array
+}
+
+let x=5;
+function whileLoop (x){
+  
+  while (x>0)
+  console.log ("done")
+}
