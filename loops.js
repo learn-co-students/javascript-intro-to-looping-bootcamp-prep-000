d8c51d17cad9d26aacb7e559a03d7c85dab7f243
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-11 06:26:12 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array){
+   for (var i = 0; i < 25;  i++) {
+     if( i == 1) {
+       array.push("I am 1 strange loop")
+     }else {
+       array.push(`I am ${1} strange loops`)
+     }
+   } return array
+}   
+
+function whileLoop(n) {
+  let countdown = n
+  while (countdown > 0) {
+    console.log(--countdown)
+  } return 'done'
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while(array.length > 0 && maybeTrue()){
+    
+  } return array 
+}
