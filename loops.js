a68af02a1f97f922df0d8c59c2f211bc2a48a5a5
Aleksandar Gakovic <algakovic@gmail.com>
2018-07-12 06:27:51 +0000

    Done.

--- loops.js
+++ loops.js
@@ -10,3 +10,19 @@ function forLoop(array){
   return array
 }
 
+function whileLoop(number){
+  while (number > 0){
+    console.log(--number)
+  }  
+  return "done"
+}
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+    }
+  do  {array.pop()
+  }
+    while (array.length > 0 && maybeTrue());
+  return array
+}
