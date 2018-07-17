bd9dca282358003146a9a3720b8986ffb4c2f163
Phakarat Campbell <jirallc@gmail.com>
2018-07-17 14:02:24 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++);
+  // return i
+  if (i === 1) {
+    return "I am 1 strange loop.";
+  } else {
+    return `I am ${i} strange loops.`;
+  }
+
+}
+
+function whileLoop(n) {
+
+}
+
+function doWhileLoop(array);
