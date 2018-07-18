8055d0cc7615bb379eda253a9228118f8b8857c8
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 18:18:16 +0000

    Done.

--- loops.js
+++ loops.js
@@ -11,3 +11,14 @@ function whileLoop (n){
   }
   return 'done';
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+  do{
+    array.pop();
+  } while(maybeTrue());
+  
+  return array;
+}
