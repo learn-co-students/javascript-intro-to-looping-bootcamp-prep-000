a58831f5663a5c5bf5d8191213b0a4054f69cf19
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 17:16:01 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,8 +2,7 @@
 
 function forLoop(array){
   for (let i=0; i<25; i++){
-    
+    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
-return  `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
-  
+return array;
 }
