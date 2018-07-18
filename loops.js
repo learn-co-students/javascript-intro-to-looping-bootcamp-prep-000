1d976bb4e38a543f53125cbd256050c80e0153f5
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 17:33:44 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,13 @@
-
-
 function forLoop(array){
   for (let i=0; i<25; i++){
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
 return array;
+}
+
+function whileLoop (n){
+  let countdown = 100;
+  while (countdown > 0){
+    console.log(--countdown)
+  }
 }
