6a5f4cbff4eb959cc874ece85b2544da18f27bef
Mai Campbell <jirallc@gmail.com>
2018-07-19 01:53:14 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,16 +1,30 @@
 function forLoop(array) {
-  for (var i = 0; i < 25; i++);
-  // return i
-  if (i === 1) {
-    return "I am 1 strange loop.";
-  } else {
-    return `I am ${i} strange loops.`;
-  }
+  for (let i = 0; i < 25; i++ ) {
+    if (i === 1) {
+      array = [...array,"I am 1 strange loop."];
+    } else {
+      array = [...array,`I am ${i} strange loops.`];
+    }
 
-}
+  }return array;
+};
 
 function whileLoop(n) {
+  let countdown = n;
 
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return 'done'
+};
+
+function maybeTrue() {
+  return Math.random() >= 0.5
 }
 
-function doWhileLoop(array);
+function doWhileLoop(array){
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
