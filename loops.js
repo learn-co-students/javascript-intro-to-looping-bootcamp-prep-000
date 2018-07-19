010610762e9e0d2febca409e59f92f71ade3d6ed
Hannah Do <hannah.hj.do@gmail.com>
2018-07-19 01:54:50 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array){
+  for(var i=0 ; i<25 ; i++){
+
+    array[i] = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
+
+  }
+  return array;
+}
+
+function whileLoop(num){
+  while(num>0){
+    console.log(num);
+    num--;
+  }
+  return "done";
+}
+
+
+// function maybeTrue() {
+//   return Math.random() >= 0.5
+// }
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(maybeTrue() && array.length >= 0);
+  return array;
+}
