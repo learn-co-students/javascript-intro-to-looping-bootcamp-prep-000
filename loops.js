d1c9cf9b13e7d413eeb45d63f84359f6735ad6ad
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-30 20:02:14 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop (array) {
+  for (let i =0; i < 25; i++) {
+    if (i === 1) {
+    array.push(`I am ${i} strange loop`);
+    } else {
+      array.push(`I am ${i} strange loops`);
+    }
+    
+  }
+  return array;
+}
+
+function whileLoop (number) {
+  while (number > 0) {
+    --number;
+    console.log(number);
+    if (number === 0) {
+      return 'done';
+    }
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop (array) {
+  do {
+    array.pop();
+  } while(array.length > 0 || maybeTrue());
+  return array;
+}
