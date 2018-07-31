763024d13135d992462fbb72a3b78777ec41e01d
Randall Libramonte <libramonte.randall@gmail.com>
2018-07-31 13:02:38 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,9 @@
+for (i = 0; i < 25; i++) {
+  if (typeof i === 'number') {
+      console.log("I am a " + ${i} + " strange loops.")
+    }
+    ) else {
+      console.log("I am a " + i + "strange loops.")
+    }
+  }
+}
