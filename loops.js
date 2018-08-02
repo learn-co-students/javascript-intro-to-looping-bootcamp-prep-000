5962e27ea340dcc89ab0c2b5abaf641efc25d1f6
Andrew Bernier <a6bernier@gmail.com>
2018-08-02 16:51:36 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) { 
+  if ( i === 1 ) {array.push(`I am 1 strange loop`) }
+  else array.push(` I am ${i} strange loop$`) }
+  return array
+}
+
+function whileLoop(n) {
+  let countdown = n
+  var whileLoop = "done"
+  while (countdown > 0) {
+  console.log(--countdown) }
+  return whileLoop
+}
+
+
+function maybeTrue() {
+  return Math.random () >= 0.5 }
+
+  do {
+    let maybeTrue = "false"
+    console.log()
+  } while (maybeTrue(array.length > 0)) 
+
+function doWhileLoop(array) {
+  return maybeTrue
+}
+
