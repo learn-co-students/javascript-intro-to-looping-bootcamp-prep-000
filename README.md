e9490085c7bb3eacdbc94616f6c6d5c3ce40b5b2
sam ditson <samditson2@hotmail.co.uk>
2018-08-03 23:00:24 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript Loops
+ so# JavaScript Loops
 
 ## Objectives
 - Build a for loop
--- loops.js
+++ loops.js
@@ -1,5 +1,22 @@
 function forLoop(array){
-  for (let i = 0; i < 25; i++){
-  if i = 1 return "I am a very strange loop.";
-  else return "I am ${i} strange loops."
-}}
+  for (let i = 0; i < 25; i++)
+  if (i === 1) {array.push("I am 1 strange loop.")}
+  else {array.push("I am ${i} strange loops")}
+  return array
+}
+
+function whileLoop(n){
+  while (n > 0){
+  console.log(n--)}
+  return "done"
+}
+
+function doWhileLoop(array){
+  function maybeTrue(){
+    return Math.random() >=
+    0.5
+  }
+  do {array.shift()}
+  while (array.length > 0, maybeTrue())
+  return array
+}
