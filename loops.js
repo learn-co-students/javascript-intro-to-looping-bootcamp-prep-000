2a16a0255dd7c31f1b2c0423d4f8cfaebca57988
Ashley Wong <ashwong04@gmail.com>
2018-08-05 18:34:45 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,40 @@
+
+function forLoop(arr) {
+  for(let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push('I am 1 strange loop.');
+    } else {
+      arr.push('I am ${i} strange loops.');
+    }
+  }
+  return arr;
+}
+
+
+
+function whileLoop(num) {
+  let i = num;
+  while(i>0) {
+    console.log(i);
+    i--;
+  }
+  return 'done';
+}
+
+
+function doWhileLoop (arr){
+  function maybeTrue(){
+    return Math.random() >= 0.5;
+    arr.pop;
+  }
+  do{ 
+    maybeTrue();
+  } 
+  while (arr.length > 0 || maybeTrue());
+  return arr;
+}
+
+
+
+
+
