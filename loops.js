2c1c5a70ea1a0a343bec318353cd8ff4c74f0739
Noah Lee <no@hlee.me>
2018-08-05 04:31:03 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,13 +1,13 @@
 function forLoop(array){
   for(i = 0; i < 25; i++){
     array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
-  } return array;
+  } return array
 }
 
 function whileLoop(n){
   while(n > 0){
     console.log(n--)
-  } return 'done';
+  } return 'done'
 }
 
 function maybeTrue(){
@@ -17,6 +17,6 @@ function maybeTrue(){
 function doWhileLoop(array){
   do{
     array.pop()
-  } while (array.length > 0 || maybeTrue());
-  return array;
+  } while (array.length > 0 || maybeTrue())
+  return array
 }
