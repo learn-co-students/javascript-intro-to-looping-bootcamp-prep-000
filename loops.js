a0faf8c98e2cd72a3405cb66f9ea79e6d501d2b4
Hank Liu <hanke.liu@gmail.com>
2018-08-07 00:14:50 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,41 @@
+// array = new Array();
+function forLoop(array) {
+for ( i = 1; i < 26; i++) {
+  if (i === 1) {
+    array[i] = `I am $(i) strange loop.`;
+    } else {
+    array[i]= `I am $(i) strange loops.`;
+    }
+}
+  return array;
+}
+
+
+// whileLoop - this is passed in my Chrome console.
+
+// var n;
+function whileLoop(n) {
+
+ while (n > 0) {
+   console.log(--n);
+}
+return 'done'
+}
+
+
+// Do-While loop
+
+// function maybeTrue() {
+
+// }
+
+function doWhileLoop(array) {
+  do {
+    array.unshift()
+  }
+  while
+  (array.length >= 0);  //maybeTrue()
+
+  }
+  console.log(array);
+}
