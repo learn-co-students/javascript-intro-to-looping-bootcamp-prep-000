8b645b7e33a696a5b0d79acde00661d66bd5f230
Preston Arnette <42144959+Prae-torius@users.noreply.github.com>
2018-08-08 21:24:48 -0400

    Update loops.js

--- loops.js
+++ loops.js
@@ -3,11 +3,11 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array.push(`I am ${i} strange loop.`);
-    }
-    else {
+    } else {
       array.push(`I am ${i} strange loops.`);
     }
   }
+  
   return array;
 }
    
@@ -17,17 +17,20 @@ function whileLoop(n) {
   while (n > 0) {
     console.log(--n);
   }
-  return ('done');
+  return 'done';
 }
 
 
-function doWhileLoop(array){
+function doWhileLoop(array) {
   var i = 1;
-    function incrementVariable() {
+  
+  function incrementVariable() {
       i = i + 1;
     }
+  
   do { 
     array.pop();
   } while (array.length > 0 && incrementVariable());
+  
   return array;
 }
