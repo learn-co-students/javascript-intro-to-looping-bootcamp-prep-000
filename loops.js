d649fd598eb070abeecda9be739a7830eb5beb0a
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 19:59:18 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 /*
 1) loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times
-
-for ([initialization]; [condition]; [iteration]) {
+ */
+for (var index = 1; [condition]; [iteration]) {
   [loopBody]
 }
-
+/*
   2) loops whileLoop(n) counts down from n to 0
 
   3) loops doWhileLoop(array) removes elements from `array` until `array` is empty or until `incrementVariable()` returns `false`:
