7eec60db4266e2acbaef0825c05845b465c0c54f
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 20:11:37 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,17 @@
 /*
 1) loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times
  */
+ 
 var array = {}
+
+function forLoop(array) {
 
 for (var index = 0; index < 25; index++) {
   array[index] = "I am ${i} strange loop${i === 0 ? '' : 's'}."
 }
+
+}
+
 /*
 
   2) loops whileLoop(n) counts down from n to 0
