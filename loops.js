3c84731a9e33b02455e8cefd505631469fa04393
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 19:49:56 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,7 @@
 /*
-1) loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times:
-     ReferenceError: forLoop is not defined
-      at Context.it (test/loops-test.js:32:28)
+1) loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`to an array 25 times
 
-  2) loops whileLoop(n) counts down from n to 0:
-     ReferenceError: whileLoop is not defined
-      at Context.it (test/loops-test.js:49:7)
+  2) loops whileLoop(n) counts down from n to 0
 
   3) loops doWhileLoop(array) removes elements from `array` until `array` is empty or until `incrementVariable()` returns `false`:
  */
