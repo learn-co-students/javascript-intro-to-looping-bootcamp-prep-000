0b9f0aa5bc13541e5c0972b7746ac6cd77b6fdd6
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-12 03:49:24 +0000

    Done.

--- loops.js
+++ loops.js
@@ -27,6 +27,17 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
+  var i = 0;
+ 
+  function incrementVariable() {
+    i = i + 1;
+  }
+  
+  do {
+    console.log('doo-bee-doo-bee-doo')
+    incrementVariable();
+  } while (i < 5);
+  
   return array
 }
  
