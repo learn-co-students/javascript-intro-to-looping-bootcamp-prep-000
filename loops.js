4fdf086435e255ecf8b135f5b7a562713c9cddf4
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-12 15:56:49 +0000

    Done.

--- loops.js
+++ loops.js
@@ -20,10 +20,8 @@ function forLoop(array) {
 var count = 0
 
 function whileLoop(count) {
-  count = 0
   while (count > 0) {
-    console.log(count)
-    count = count - 1
+    console.log(--count)
   }
   return 'done'
 }
