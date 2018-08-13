76f12eb64d2181559f3cbfcc1fe6fc14f0764772
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-13 21:04:17 +0000

    Done.

--- loops.js
+++ loops.js
@@ -5,6 +5,8 @@ function forLoop(array) {
   for (var i = 0; i < 25; i++) { 
     if (i === 1) {
       array[i] = `"I am 1 strange loop${i === 1 ? 's' : ''}."`
+    } else {
+      array[i] = `"I am 1 strange loop${i === 2 ? 's' : ''}."`
     }
   }
    
