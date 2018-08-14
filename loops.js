60380ce81b803d735d5d3187d93287de80202c9c
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-14 16:26:17 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,7 +9,7 @@ function forLoop(array) {
     if (i === 1) {
       array.push("I am 1 strange loop.")
     } else {
-      array.push(`"I am ${i} strange loops."`)
+      array.push(`I am ${i} strange loops.`)
     }
 
   } 
