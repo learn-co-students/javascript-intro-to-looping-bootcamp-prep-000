e4eb4597b55933562d530b05a9546693f32bb74c
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-14 02:52:29 +0000

    Done.

--- test/loops-test.js
+++ test/loops-test.js
@@ -32,7 +32,7 @@ describe('loops', () => {
       const strangeArray = forLoop(array)
       const testArray = strangeArray.slice(array.length)
 
-      let first = "I am 11 strange loops."
+      let first = "I am 1 strange loop."
       let rest = "I am 24 strange loops."
         
       expect(strangeArray[11]).to.equal(first)
