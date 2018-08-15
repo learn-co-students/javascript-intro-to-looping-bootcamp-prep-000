e89941f2804281324888584cf9afe3950280fb63
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-15 19:53:29 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,7 +3,5 @@ for (i = 0; i < 25; i++) {
   if(i===1){
     array.push("I am 1 strange loop")
     } else{
-  array.push(I am ${i} strange loop.”)
-     
-      }
-}}
+  array.push(`I am ${i} strange loop.`)
+    }}}
