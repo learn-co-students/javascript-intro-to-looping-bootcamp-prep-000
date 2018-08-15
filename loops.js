a82fcab2e321e4952d81ac7749293cf91197d64d
Pearl Zeng <zhuzeng0418@gmail.com>
2018-08-15 14:59:53 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {array.push(`I am 1 strange loop.`)}
+    else {array.push(`I am ${i} strange loops.`)}
+  }
+  return array
+}
+
+function whileLoop(n) {
+  let countdown = n
+  while (countdown > 0) {
+    console.log(countdown)
+    countdown--
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  let i = 0
+  
+  function incrementVariable() {
+    i = i + 1;
+  }
+  
+  do {
+    array.shift()
+  } while (array.length > 0 && incrementVariable())
+  
+  return array
+}
