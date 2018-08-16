1a0a3dc7c4e2256a9794753be0ad5d5a7c5d857b
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-16 23:38:55 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,16 @@
 function forLoop(array) {
-  for ( var i = 0; i < 25; i++) {
+  for ( let i = 0; i < 25; i++) {
     if (i == 1) {
-      array.push("I am 1 strange loop.")
+      array.push("I am 1 strange loop.");
     } else {
       array.push("I am ${i} strange loops.");
-      i++
     }
  }
 }
 
+function forLoop(array) {
+  for ( let i = 0; i < 25; i++) {
+    array.push("I am ${i} strange loops.");
+    i++;
+  }
+}
