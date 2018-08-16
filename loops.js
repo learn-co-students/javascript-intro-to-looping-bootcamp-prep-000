f924c59bec09ded3012a71b910c251500152edfe
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-16 23:24:08 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,15 +1,11 @@
 function forLoop(array) {
   for ( var i = 0; i < 25; i++) {
     if (i == 1) {
-      array.push(["I am 1 strange loop."])
+      array.push("I am 1 strange loop.")
     } else {
-      array.push(["I am ${i} strange loops."]);
+      array.push("I am ${i} strange loops.");
       i++
     }
  }
 }
 
-function elementpush(array) {
- array.push(["I am string"])
- console.log(array)
-}
