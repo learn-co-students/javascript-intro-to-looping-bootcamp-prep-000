fe298b5eb7eefd7cbd54a659fcab90ccfd65b9e8
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 02:10:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,47 @@
+ /* 
+function forLoop(crackers){
+  for (var i = 1; i < 25; i++) {
+    if (i === 1) {
+      crackers.push("I am" + i + "strange loop.");
+      
+      }
+    else {
+      crackers.push(`"I am ${i} strange loops."`);
+    }
+  }
+  return crackers;
+}
+
+    */
+function forLoop(array) {
+  
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am " + i + " strange loop.")
+      }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+  return `done`
+}
+
+
+function doWhileLoop(array){
+  do {
+    array.pop ();
+   
+    }
+    while (array.length > 0){
+}
+return array
+}
+
