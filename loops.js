1f83c756ac9f657358be075041d1aea123b33589
Sarah Pai <pai.sarah89@gmail.com>
2018-08-25 21:52:02 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,30 +1,53 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
-    if (i === 1){
-      array.push (`I am ` + i + ` strange loop.`)}
-  else {
-      array.push (`I am ${i} strange loops.`)}
-}
-return array
-}
-
-
-function whileLoop(n){
-  while(n > 0){
-  console.log (--n)
+    if ( i === 1){
+    array.push(`I am ` + i + ` strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
   }
-   return 'done';
+ return array;
 }
+
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+return 'done';
+}
+
+
+
+function doWhileLoop(array){
+  var i = 1;
+
+function incrementVariable() {
+  i = i + 1;
+}
+ 
+do {
+  console.log(array.length > 0 && incrementVariable());
+} while (i < 5);
+
+  return array
+}
+
 
 function doWhileLoop(array) {
+  var i = 1;
   
-  do {
+  function incrementVariable() {
+      i = i + 1;
+    }
+  
+  do { 
     array.pop();
-  } while (array.length > 0 && incrementVariable()) {
-  }
+  } while (array.length > 0 && incrementVariable());
   return array;
 }
 
-function incrementVariable() {
-  return Math.random() >= 0;
-}
+//function incrementVariable() {
+//return Math.random() >= 0;
+// }
