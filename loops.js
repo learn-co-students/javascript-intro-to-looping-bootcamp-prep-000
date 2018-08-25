9cd8bc7bf2297a7b6ac61f8caf560797c7771b75
Caren Rz <anruiz110@gmail.com>
2018-08-25 07:41:01 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,35 @@
+//anruiz23
+
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    if(i ===1){
+      array.push(`I am ${i} strange loop.`);
+    }else{
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array 
+}
+
+
+function whileLoop(number){
+  while(number > 0){
+    console.log(--number);
+  }
+  return "done"
+}
+
+
+
+function incrementVariable(i) {
+  i = i + 1;
+}
+
+function doWhileLoop(array){
+  var i = 0;
+  do{
+    array.pop()
+    
+  }while(array.length > 0 && incrementVariable(i));
+  return array
+}
