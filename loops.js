03e897092115e89cd9f46bd8748f258205249c69
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-08-27 20:35:52 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array) {
+  for (var i=0; i < 25; i++) {
+    if (i===1){
+      array.push("I am 1 strange loop.")}
+      else {
+         array.push("I am " + i + " strange loops.");
+    }
+ } 
+ return array;
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(--n > 0);
+  }return 'done';
+}
+
+
+function doWhileLoop(array) {
+  var i = 0;
+  function incrementVariable() {
+  i++;
+}
+ do { array.shift();  
+      incrementVariable();
+ }
+ 
+   while (array.length > 0 && i < array.length);
+ 
+ return array;
+}
