dba58d26fe50bede4e768b5dfdffb81c0f035f36
gr98577 <gr98577@umbc.edu>
2018-08-28 17:26:06 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,27 @@
 function forLoop(arr){
   for(i = 0; i < 25; i++){
-    if(i === 1){
-      arr.push("I am 1 strange loop.");
-    }
-    else{
-      arr.push(`I am ${i} strange loops.`);
-    }
-    
+   if(i === 1){
+     arr.push("I am 1 strange loop.");
+   }
+   else{
+     arr.push(`I am ${i} strange loops.`);
+   }
   }
+  return arr;
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(n);
+    n--;
+  }
+  return "done"
+}
+
+function doWhileLoop(arr){
+  do{
+    arr.length -= 1;
+  }while(arr.length > 0)
+  
+  return arr;
 }
