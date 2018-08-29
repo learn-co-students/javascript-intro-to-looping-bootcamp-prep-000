4022cd76c88948686a2ca03e1dbba37bef436f5f
Leslie Turis <lturis@gmail.com>
2018-08-29 00:15:47 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,27 +1,36 @@
+//for loop
+
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
+  for(let i = 0; i < 25; i++) {
+    if (i === 1){
       array.push('I am 1 strange loop.');
     } else {
-      array.push('I am ${i} strange loops');
+      array.push(`I am ${i} strange loops.`)
     }
   }
-return array;
+  return array;
 }
 
-function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n)
+//while loop
+
+function whileLoop(num) {
+  while(num > 0) {
+    console.log(--num);
   }
-return 'done';
+  return 'done';
+}
+
+
+//do-while loop
+
+let i = 0;
+function incrementVariable() {
+   i++;
 }
 
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5;
-  }
-  do {
-    array.length--;
-  } while (array.length = 0 && maybeTrue());
+   do {
+     array.length--;
+   } while (array.length && !incrementVariable());
     return array;
 }
