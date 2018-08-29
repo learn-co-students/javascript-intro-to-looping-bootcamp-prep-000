d000b6914a4d7b70eeb81e39680ae3741e44bbcc
Manju Thomas <kudukacar@hotmail.com>
2018-08-29 21:10:13 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(array) { 
+for (let i = 0; i < 25; i++) {
+  if (i == 1) { array.push("I am 1 strange loop."); }
+  else { array.push("I am 24 strange loops."); }
+}
+return array;
+}
+function whileLoop(n) {
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return "done";
+}
+function doWhileLoop(array) {
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+  }
+  do {
+    array.shift();
+    incrementVariable();
+  } while (array.length > 0);
+  return array;
+}
