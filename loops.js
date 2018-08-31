236bcecfd37b3e810088cdf6af7749d8ff9af2ff
satyasinha94 <satyasinha94@gmail.com>
2018-08-31 21:01:03 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,26 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-     array += "I am " + `${i}` + " strange loop."
+    if (i <= 1) {
+     array.push ("I am " + `${i}` + " strange loop.")
 }
+  else if (i > 1) {
+    array.push ("I am " + `${i}` + " strange loops.")
+  }
+}
+return array;
+}
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  if (n == 0) {
+    return "done";
+  }
+  }
+}
+function doWhileLoop(array) {
+  do {
+    array.pop ();
+  }
+  while (array.length > 0)
 return array;
 }
