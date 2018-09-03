0685364f99417740e7e1897ef66c755951832675
ELIZABETH ALVARADO <lily21418@yahoo.com>
2018-09-03 02:33:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,13 @@ for (let i = 0; i < 25; i++) {
 }
 return array
 }
+
+
+function whileLoop(n) {
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return "done";
+}
+
