30352567ccbaf6b704fb58a4c50c10652c85f917
Alisa McCormick <muntananuchat@gmail.com>
2018-09-04 22:02:29 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,27 +1,31 @@
 function forLoop(array) {
-  for(var i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push(`I am 1 strange loop.`)
+      array.push("I am 1 strange loop."); 
     } else {
       array.push(`I am ${i} strange loops.`)
     }
-}
-return array
-}
-
-function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n)
   }
-  return "done"
+  return array  
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return 'done'
 }
+
 
 function doWhileLoop(array) {
-  do { array.pop()
-  } while (array.length > 0 && maybeTrue())
- return array
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+  }
+  do { 
+    array.shift();
+  } while (array.length > 0 && incrementVariable());
+  return array
 }
+
+
