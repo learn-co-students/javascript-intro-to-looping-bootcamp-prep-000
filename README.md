9fc62c6e1e0d48b35db0b21dd96e7c50e47a8fe9
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 05:54:21 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript Loops
+claerclae# JavaScript Loops
 
 ## Objectives
 
---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) array.push(`I am ${i} strange loop.`);
+     else array.push(`I am ${i} strange loops.`)
+  }
+  return array;
+}
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.shift();
+  } while(array.length > 0);
+  return array;
+}
