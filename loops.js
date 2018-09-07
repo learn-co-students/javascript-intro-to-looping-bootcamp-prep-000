f6e798bf7910635dd68744494dcec10bf6253045
Carlo Fernando <ca.fernando415@gmail.com>
2018-09-07 14:27:04 +0000

    Done.

--- loops.js
+++ loops.js
@@ -24,6 +24,6 @@ function doWhileLoop(array) {
   
   do {
     array.pop();
-  } while (array.length > -1 && incrementVariable());
+  } while (array.length > 0 && incrementVariable());
   return array;
 }
