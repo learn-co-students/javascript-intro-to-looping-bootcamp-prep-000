b8bcdaeccdd151b2f595a19411fb6a580c4e6570
Jessica Gladstone <jcg321@comcast.net>
2018-09-12 15:12:54 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,15 +1,16 @@
-   function forLoop(array) {
-     const arr = [];
-    	for (let i = 1; i < 26; i++) {
-  	  
-  		if (i === 1) {
-  			arr.push("I am 1 strange loop.");
-  		} else  {
-  			arr.push(`I am ${i} strange loops`);
-  		}
-  	}
-    return arr;
-  }
+   
+  
+  function forLoop(array) {
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+
+	return array;
+}
   
   function whileLoop(n) {
     let countdown = n;
@@ -19,6 +20,14 @@
     }
     return "done";
   }
-  
-  
+  function doWhileLoop(array) {
+   let i = array;
+   console.log(i);
+    do {		
+     array.splice(1);
+    }
+    while (i > 0 && array.length -1);
+	console.log (array);
+	return array;
+  }
   
