4b5ab4de5f95a18cf8c155068ab843ebffb599b7
persistent-partial-7850 <bhorowitz2014@gmail.com>
2018-09-12 04:05:26 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+      if (i === 1) {
+      array.push(`I am 1 strange loop.`);
+    } else {
+      array.push( `I am ${i} strange loops.`);
+    }
+  }
+  return array;
+} 
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--);
+  }
+  return 'done';
+}
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && incrementVariable());
+  return array;
+}
