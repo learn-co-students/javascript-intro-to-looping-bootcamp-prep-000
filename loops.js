97d6496020f9d25a162ddf15c0e1606e17bed2c1
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-24 00:04:17 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,23 @@ for (let i = 0; i < 25; i++) {
 }
 return array
 }
+
+
+function whileLoop (n) {
+  while (n > 0) {
+    console.log(--n) 
+  }
+  return ('done')
+}
+
+function incrementVariable(i) {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {array.pop()
+  }
+    while (array.length > 0 && incrementVariable()) 
+  return array
+}
+
