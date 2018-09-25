2356e5291578d09786b012a6c0aa7ef5c80217ac
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-25 12:23:05 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,37 @@
+
+function forLoop(array) {
 for (let i = 0; i < 25; i++){
   if(i === 1){
-  console.log("I am 1 strange loop.")
+  array.push(`I am 1 strange loop.`)
   }
   else{
-    console.log("I am ${i} strange loops.")
+    array.push(`I am ${i} strange loops.`)
   }
 }
+return array;
+}
+
+function whileLoop(number){
+  let countdown = number;
+  
+  while(countdown > 0){
+    countdown = countdown - 1;
+    console.log(countdown);
+  }
+  return "done";
+  
+}
+
+function doWhileLoop(array){
+    function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+}
+}
+
+
+
