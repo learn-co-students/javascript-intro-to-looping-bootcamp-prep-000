ac249df294605708925dd2c3b57f4e5e079f252f
Brian <briandsalemi@gmail.com>
2018-09-28 21:17:41 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,17 @@ function forLoop(array) { for (let i = 0; i < 25; i++) {
 function whileLoop(n) {  while (n > 0) { console.log(--n); }
 
 return 'done'}
+
+function doWhileLoop(array) {
+  var i = 0;
+  function incrementVariable() {
+  	i = i + 1;
+	}
+  do {
+    array.pop();
+    incrementVariable();
+    }
+    while (i < array.length);
+      
+      return array;
+      }
