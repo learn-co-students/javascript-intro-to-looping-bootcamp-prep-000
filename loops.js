b5d92bcf9603a68e83a15559c17240e57ef68ed0
Taylor Watson <taylorwatsonb@gmail.com>
2018-10-03 01:30:25 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,22 @@
+var names = ["Taylor", "Marcia", "Kamille", "Amber", "Travis"];
+	var text = "";
+    
+    for (var i = 0; i < names.length; i++) {
+    	text += names[i] + "<br>";
+    }
+
+	var names = ["Taylor", "Marcia", "Kamille", "Amber", "Travis"];
+	var text = "";
+	var x = 0;
+    
+    while (x < names.length) {
+    	text += names[x] + "<br>";
+    	x++;
+    }
+var x = 0;
+
+	do {
+		text += names[x] + "<br>";
+    	x++;
+
+	} while (x < 10);
