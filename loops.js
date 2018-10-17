93bce5721986f617ddeeb3c8370acddf107a66d9
Victoria L Sumner <vicsumner@gmail.com>
2018-10-17 19:57:34 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array){
+  for (let i = 1; i< 25;i++){
+    array.push ("I am a strange loop.");
+  }
+  if (i === 1){ 
+  console.log("I am 1 strange loop.")
+  }
+  else {
+  console.log ("I am ${i} strange loops.")
+}}
