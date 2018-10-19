6c5958645b1ad220791e39cfff892a12c5398a56
Wil Cook <wil.cook.is@gmail.com>
2018-10-19 15:05:21 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop (array) {
+  for (let i = 1; i < 26; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops`)
+    }
+  console.log(array);
+  }
+}
+
+let test = [];
+forLoop(test);
