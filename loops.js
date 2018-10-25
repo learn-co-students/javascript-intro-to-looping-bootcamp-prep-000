56c8dd3451125effaa04a56c081a16eeb354ca98
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-25 23:36:22 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i != 1) {
+       array.push("I am " + i + " strange loops.")
+    } else {
+       array.push("I am 1 strange loop.")
+      }
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    number = number - 1
+    console.log(number)
+  }
+  return ('done')
+}
+
+function doWhileLoop(array) {
+  array = array.length <= 9
+  var i = 9;
+  do {
+    function incrementVariable() {
+    i = i - 1
+    }
+  } while ( array.length > 0 && incrementVariable())
+  return ('length')
+}
