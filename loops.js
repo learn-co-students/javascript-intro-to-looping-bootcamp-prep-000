ef63a4c6f55d00cce5eef1b8682a3c563720b714
Carol Gardiner <carolan.gardiner@gmail.com>
2018-10-28 18:47:43 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,23 @@
+ 
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if(i === 1) {
+      array.push(`I am ${i} strange loop.`);
+    }else{
+       array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+    
+function whileLoop(number) {
+  while(number > 0) {
+    console.log(--number);
+  }
+  return "done";
+}
+
+function doWhileLoop {
+  do
+}
