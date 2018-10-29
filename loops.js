ae367183d365a708cc0d128f1488faec46978a53
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-29 06:01:44 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,26 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
-    if (let i == 1) {console.log("I am 1 strange loop.")
-    } else { console.log(" I am ${i} strange loops.")}
+    if (i === 1) {array.push(`I am 1 strange loop.`)
+  } else { array.push(`I am ` +i+ ` strange loops.`)}
   }
   return array
 }
+
+function whileLoop(n){
+  let countdown = n;
+  while (countdown > 0){
+    console.log(--countdown);
+  }
+  return `done`
+}
+
+function doWhileLoop(array){
+  let i = 0;
+  function incrementVariable(){
+    i = i + 1 }
+  do { array.pop()
+  } while (array.length > 0 && incrementVariable())
+  return array
+}
+
+console.log(doWhileLoop())
