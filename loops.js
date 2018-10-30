7eb27711c8351dab93532c4630fd5088428a9e82
Wil Cook <wil.cook.is@gmail.com>
2018-10-30 12:47:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,30 @@
 function forLoop (array) {
-  for (let i = 0; i < 26; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.");
     } else {
-      array.push(`I am ${i} strange loops`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
   return array;
+}
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(--num)
+  }
+  return 'done';
+}
+
+var i = 0 
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    return array.splice(0,1);
+    incrementVariable();
+  } while ( i < array.length )
 }
