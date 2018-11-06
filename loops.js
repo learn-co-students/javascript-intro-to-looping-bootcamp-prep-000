6cd04ad4dc676c9fd9ba094acf83c9ab3e80b9c9
Cheng Vue <cvu3@ucsc.edu>
2018-11-06 18:02:00 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+
+  return array;
+}
+
+
+function whileLoop(n) {
+  while (n >= 0) {
+    if (n === 0) {
+      return 'done';
+    }
+    console.log(n);
+    n -= 1;
+  }
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && incrementVariable());
+
+  return array;
+}
