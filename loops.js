469c5c7c586e80d632840630bf87dacd63ad53f5
H B <phajib@gmail.com>
2018-11-15 22:43:34 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,35 @@
+/*forLoop(array) function*/
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    }
+    else{
+      array.push(`I am ${i} strange loops.`)
+    }
+  }return array
+}
+
+/*whileLoop(n) function
+counts down from n to 0*/
+function whileLoop(n){
+  while(n > 0){
+    console.log(--n)
+  }
+  return n = 'done'
+}
+
+/*doWhileLoop(array) function
+removes elements from array until array is empty or until
+incrementVariable returns false*/
+function incrementVariable(){
+  i = i + 1
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop()
+    return array
+  }while( array.length > 0 && incrementVariable())
+  return [array]
+}
