680b49f96660ff32c82e296fc8077ab124666906
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-17 04:19:10 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
-    array.push("I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     }
     else {
-     array.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
   return array;
@@ -17,26 +17,10 @@ function whileLoop(number) {
   return 'done';
 }
 
-function doWhileLoop(array) {
-  var i = 1;
-  
-  function incrementVariable() {
-    i = i + 1;
-  }
-  
-  do {
-    array.pop();
-  }
-  while (array.length > 0 && incrementVariable());
-  return array;
-}
+
 
 /*
-function makeArray() {
-  const array = []
-  const t = Math.floor(Math.random() * 10)
-
-  for (let i = 0; i < t; i++) {
+for (let i = 0; i < t; i++) {
     array.push("I am a strange loop.")
   }
 
