08e0a772f8b59a529f902e6b48ba80c39d75764d
Naenae L <oneprincesslea@gmail.com>
2018-11-20 19:37:59 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
-for (var i = 1; i <= 25; i++) {
-console.log (`I am ${i} strange loops.`); 
-  if (i == 1) {
-    console.log ("I am 1 strange loop"); 
-  } 
+var number = 25; 
 
-}
+function whileLoop(number){
+  while (number > 0) {
+    console.log(number--); 
+  }
+  if (number == 0){
+    console.log("Done.")
+  }
+}  
+
+whileLoop(25); 
