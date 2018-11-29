b7a2e52d59af2132e3980e97a644133fe6bbdb89
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-11-29 13:39:37 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,31 @@
 function forLoop(array) {
-for (let i = 0; i < 25; i++) {
+  for (let i=0; i<25; i++) {
+    if (i==1){
+  array.push("I am 1 strange loop.")
 }
-  if i = 1
-  return ("I am 1 strange loops.")
   else {
-    return ("I am ${i} strange loops.")
+    array.push(`I am ${i} strange loops.`)
   }
+}
+return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+  return "done"
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array){
+  do {array.pop(incrementVariable(array))
+  }
+  while (array.length > 0 && incrementVariable());
+  return array
+}
