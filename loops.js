092397f81cf2740b5e4607d508108b6c821d6aca
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-29 22:47:07 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++); {
+  for (let i = 0; i < 25; i++) {
     if (i == 1) {
       array.push("I am 1 strange loop.");
     }
@@ -7,5 +7,26 @@ function forLoop(array) {
       array.push(`I am ${i} strange loops.`);
     }
   }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n !== 0)
+  {
+    console.log(--n);
+  }
+  return "done";
+}
+
+function doWhileLoop(array) {
+  var i = 0;
+  function incrementVariable() {
+  i = i + 1;
+  }
+  do {
+    incrementVariable();
+    array.pop();
+  }
+  while (incrementVariable() && array.length > 0);
   return array;
 }
