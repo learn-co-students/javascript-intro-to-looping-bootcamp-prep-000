3b1208ea276ff1b105537037b625f8ad8344cecf
Noah Sragow <nsragow@gmail.com>
2018-12-04 17:59:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array)
+{
+  for(i = 0; i < 25; i++)
+  {
+    array.push(`I am ${i} strange loop${(i === 0)||(i === 1) ? '' : 's'}.`)
+  }
+  return array
+}
+function whileLoop(n)
+{
+  while(n > 0)
+  {
+    console.log(n--)
+  }
+  return "done"
+}
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array)
+{
+  
+  do{
+    array.pop()
+
+  }while(incrementVariable() && array.length > 0)
+  return array
+}
