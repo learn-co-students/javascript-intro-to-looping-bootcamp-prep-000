1085ea9760bdac74b998a05f8fd81660348b12db
Cassidy Samelian <csamelian@gmail.com>
2018-12-10 00:44:07 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+  if (i === 1){
+  array.push('I am 1 strange loop.');
+   }
+  else {
+  array.push(`I am ${i} strange loops.`);
+  }
+  }
+  return array;
+}
+
+function whileLoop(n){
+  while (n > 0)
+  {
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+
+function doWhileLoop(array){
+  function incrementVariable()
+  {
+    array.length--;
+  }
+  do {
+    incrementVariable();
+  } 
+  while (array.length > 0);
+  return array;
+}
