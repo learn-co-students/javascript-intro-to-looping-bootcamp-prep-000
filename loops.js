b52c31bd285904f937cb515a21f81474f2727411
Paris Dolfman <parisdolfman@gmail.com>
2018-12-11 20:10:01 +0000

    Done.

--- loops.js
+++ loops.js
@@ -4,8 +4,8 @@ function forLoop(array) {
   if (i === 1) {
   array.push(`I am ${i} strange loop.`);
 } else {
-   array.push(`I am ${i} strange loops.`);
+   array.push(`I am ${i} strange loops.`); //${ } interpolation only works when using backticks on string
 }
 }
-return array
+return array // has to be outside of for loop; when inside for loop, loop stops once hitting return
 }
