28b52f659ea074cc13e365f13dcb3001246dc667
Paris Dolfman <parisdolfman@gmail.com>
2018-12-11 19:47:59 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
 
 function forLoop(array) {
-  for (let i = 0; i < 25; i++)
+  for (let i = 0; i < 25; i++) {
   if (i === 1) {
-  array.push("I am" ${i} "strange loop.");
+  array.push(`I am ${i} strange loop.`);
 } else {
-   array.push("I am" ${i} "strange loops.");
+   array.push(`I am ${i} strange loops.`);
 }
-  return array
+}
+return array
 }
