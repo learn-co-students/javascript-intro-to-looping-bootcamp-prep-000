f1e774234e148aedd3531e3ebf3f2bef549ca6ba
Charles Treloar <cwtreloar@gmail.com>
2018-12-13 15:56:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    
+    if (i === 0) {
+      return `"I am ${i} strange loop."`;
+    }
+    
+    else {
+      return `"I am ${i} strange loops."`;
+    }
+  }
+}
