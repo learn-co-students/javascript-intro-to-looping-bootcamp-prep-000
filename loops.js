3832544e8be7197c11d4d8ae4cb90d01ec49e09b
Charles Treloar <cwtreloar@gmail.com>
2018-12-13 17:22:08 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,12 +1,37 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     
-    if (i === 0) {
-      return `"I am ${i} strange loop."`;
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`);
     }
     
     else {
-      return `"I am ${i} strange loops."`;
+      array.push(`I am ${i} strange loops.`);
     }
   }
+return array;
+}
+
+function whileLoop(n) {
+  let countdown = n;
+  
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return 'done';
+}
+
+function doWhileLoop(array) {
+  var i = 0;
+  
+  function incrementVariable() {
+    i = i + 1;
+  }
+  
+  do {
+    array.pop;
+    --array.length;
+  } while (array.length > 0 && incrementVariable());
+  
+  return array;
 }
