62aff4c989f95f7837077a17f9e80b62c2f1601d
Nicholas Stano <nicholasstano@gmail.com>
2018-12-19 01:51:17 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,22 +1,20 @@
-function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n)
+function forLoop(array) {
+  for (let i = 0; i < 25; i++)
+  {
+    if (i===0) {
+      array.push("I am 1 strange loop.")
+    }
+    else 
+    array.push(`i am ${i} strange loops.`)
   }
-  return 'done'
-  
+  return array
 }
 
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-  if (i===0) {
-    array.push("I am 1 strange loop.")
+function whileLoop(n) {
+  while (n > 0) {
+  console.log(--n) 
   }
-  else {
-    array.push(`I am ${i} strange loops.`)
-  }
-  
-  }  
-  return array
+return 'done'
 }
 
 function doWhileLoop(array) {
