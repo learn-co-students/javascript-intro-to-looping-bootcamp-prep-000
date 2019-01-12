835a60c931f2bb7fa1d3a0fc1d31ce7622cd6b87
Joan in DC <joanrigdon@gmail.com>
2019-01-12 22:33:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,31 +1,41 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++){
-    if (i === 1) {
+  for (let i=0; i<25; i++) {
+    if (i===1) {
       array.push(`I am 1 strange loop.`);
     } else {
       array.push(`I am ${i} strange loops.`);
-      }
     }
-    return array;
   }
-  
+  return array;
+}
 
-let number = 75;
 function whileLoop(number) {
-while (number > 0) {
-  console.log(--number);
+  while (number > 0) {
+    console.log(--number);
     }
   return "done";
-}
+  }
 
 
 function doWhileLoop(array) {
-   function maybeTrue() {
-    return Math.random() >= 0.5
+  var i = 0
+  function incrementVariable() {
+    i = i + 1;
   }
-   do {
+  do {
     array.pop();
-  } while (array.length > 0 && maybeTrue());
-   return array;
+  } while (array.length > 0 && incrementVariable());
+  return array
 }
+
+
+// function doWhileLoop(array) {
+//   function maybeTrue() {
+//     return Math.random() >= 0.5
+//   }
+//   do {
+//     array.pop();
+//   } while (array.length > 0 && maybeTrue());
+//   return array;
+// }
 
