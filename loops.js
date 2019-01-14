c2fb0d81b23637969ddc9b357c4b5702cd7038ee
Paul Tudora <tudora307@gmail.com>
2019-01-14 17:25:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //function forLoop(array) {
   //for (let i = 0; i < 25; i++) {
   //if(i===1){array.push("I am "+ i + " strange loop.")}
@@ -7,3 +8,14 @@
   //return array
   
 //}
+=======
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  if(i===1){array.push("I am "+ i + " strange loop.")}
+  
+  else{array.push("I am "+ i +" strange loops.")}}
+
+  return array
+  
+}
+>>>>>>> 9d5c4cce09e65f777dfae5f445398cb251ab692f
