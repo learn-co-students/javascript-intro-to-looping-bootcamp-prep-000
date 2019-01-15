0c893e516e12dc71ff10b18cb714f3f6b994cb8e
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-01-15 07:15:18 +0000

    Done.

--- loops.js
+++ loops.js
@@ -15,14 +15,3 @@ function whileLoop(num){
   }
   return "done"
 }
-
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
