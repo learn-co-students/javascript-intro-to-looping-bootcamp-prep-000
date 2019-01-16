0db9188ee4e48e813c71273dd9786d7cf67ec5bf
Lucas Teles de Lima <lucastlima86@gmail.com>
2019-01-16 21:13:18 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i <= 1 ? '' : 's'}.`)
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  let i = 0;
+
+  function incrementVariable() {
+    i++
+  }
+
+  do {
+    array.pop()
+    incrementVariable()
+  } while (array.length > 0 && incrementVariable());
+
+  return array
+}
