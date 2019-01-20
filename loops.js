757a5f9fd4b8a0c03d556dab7a603d439d1fbc63
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-20 06:37:24 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array){
+  for(let i = 0;i<25; i++){
+    if(i===1){
+      array.push("I am 1 strange loop.")
+    }else{
+    array.push("I am "+i+" strange loops.")
+  }
+}
+return array;
+}
+function whileLoop(initalCount){
+  while(initalCount>0){
+    console.log(--initalCount)
+  }
+  return 'done'
+}
+var i = 0;
+function incrementVariable() {
+  i = i + 1;
+}
+function doWhileLoop(array){
+  do{
+    array.pop();
+    incrementVariable();
+} while(array.length>0 && incrementVariable());
+return array;
+}
