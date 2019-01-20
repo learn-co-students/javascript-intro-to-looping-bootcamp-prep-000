56f7f8bb92bc96f1fc6eec7c3c6029523b647488
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-20 17:39:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+    for (var i = 0; i < 25; i ++) {
+      if (i > 1);
+        console.log("I am ${i} strange loops.");
+        return array
+      else
+        console.log("I am 1 strange loop.")
+        return array
+    }
+}
