4c39ecb264ecb5d39f67fffca19e121aa3ecc6f9
Harry Bigos <hrbigos@gmail.com>
2019-01-26 00:03:20 +0000

    Done.

--- loops.js
+++ loops.js
@@ -24,13 +24,15 @@ function whileLoop(n) {
 }
 
 
-var i = 0;
+
+
+function doWhileLoop(array) {
+  var i = 0;
 
 function incrementVariable() {
   i = i + 1;
 }
 
-function doWhileLoop(array) {
   do { 
     array.pop()
   }
