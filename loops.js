3ae74d30407fc72e9056ff93707b0423237a591e
Milan Dhar <milan.dhar@gmail.com>
2019-02-14 19:06:58 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop(array) {
+  for(let i = 0; i < 25; i++) {
+    if (i = 1) {
+      [...array, "I am 1 strange loop."]
+    }
+    else {
+     [...array, "I am ${i} strange loops."] 
+    }
+  }
+}
+
+function whileLoop(number) {
+  while(number > -1) {
+    if (number = 0) {
+      return 'done'
+    }
+    else {
+    console.log(number)
+    number --
+    }
+    
+  }
+  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  function doWhileLoop(num) {
+    do {
+     console.log("I run once regardless") 
+    } while 
+    (incrementVariable() < num);
+  }
+}
