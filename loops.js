5d643757c85f2488bba2245e8f2d6d23911babfe
Michael Maggs <michaelmaggs@gmail.com>
2019-02-17 00:47:10 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,29 +1,29 @@
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-  if(i === 1) {
-  array.push(`I am ${i} strange loop.`);
-  } else {
-  array.push(`I am ${i} strange loops.`);
+function forLoop (array) {
+  for (i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n >= 0) {
+   if (n > 0) {
+     console.log(n)
+   } else {
+     return "done"
+   }
+    n--
   }
 }
- return array;
-}
 
-function whileLoop(num) {
-	while (num > 0) {
-	console.log(num)
-	num--;
-	}
-	return "done";
-}
-
-function doWhileLoop (integer) {
-  function incrementVariable() {
-    integer -= 1;
-  }
-
-do { 
-  console.log("I run once regardless")
-  incrementVariable()
-} while (integer > 0)
+function doWhileLoop(num) {
+  
+  do {
+    console.log("I run once regardless");
+    num--
+  } while (num > 0);
 }
