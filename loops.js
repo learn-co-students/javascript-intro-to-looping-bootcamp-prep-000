3b2938346e2b46c87d87286a932a2c96288648b3
Aodhan Vanderbilt <tamegorilla@gmail.com>
2019-03-31 16:57:27 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,38 +1,31 @@
-// // looping
-// for (var i = 1; i < 100; i++) {
-//   console.log("Hello World the " + i + " time");
-// }
-
 function forLoop(array){
-  for (i = 0; i < 25; i++ ){
-    if (i === 1){
-      array.push('I am 1 strange loop.');
-    } else {
-      array.push(`I am ${i} strange loops.`);
-    }
-  }
-  return(array) // you must return the answer by calling the argument / object. In this case array. 
+  for( i = 0; i < 25; i++ ) {
+    if(i === 1){
+    array.push('I am 1 strange loop.');
+  } else {
+    array.push(`I am ${i} strange loops.`);
+}
+}return(array)
 }
 
-function whileLoop(num){
-  while(num > 0) {
-    console.log(--num);
+function whileLoop(num) {
+  while (num > 0) {
+   console.log(--num)
   }
-  return ('done')
+  return('done')
 }
 
-function doWhileLoop(num){
+function doWhileLoop(int) {
   var i = 0;
   
-  function incrementVariable(){
+  function incrementVariable() {
     i = i + 1;
     return i;
   }
-    do {
-    console.log("I run once regardless.")
-      
-    } while (incrementVariable() < num) 
-    // if num is greater than incrementVariable() it will step through one more time. so while (incrementVariable() <= num) would go one more loop. Corect 
-    // this by incrementVariable() < num) which will stop the increment at the exact value of num instead of incrementing one more iteration.
+  do {
+    console.log('I run once regardless')
+  }
+  while(incrementVariable(int) < int)
 }
-
+ // if int is greater than incrementVariable() it will step through one more time. so while (incrementVariable() <= int) would go one more loop. Corect 
+    // this by incrementVariable() < int) which will stop the increment at the exact value int instead of incrementing one more iteration.
