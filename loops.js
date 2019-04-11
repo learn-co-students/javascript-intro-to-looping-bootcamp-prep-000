e4c84f11d6a00c037cf19fe213ecdc1858a67386
Beto Dute <betodute@gmail.com>
2019-04-11 02:09:10 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+  function forLoop(array) {
+  for ( var i = 0; i < 25; i++) {
+  if (i === 1) { 
+    array.push('I am 1 strange loop.');
+  } else { 
+    array.push(`I am ${i} strange loops.`);
+  } 
+}
+return array;
+}
+
+function whileLoop(n) {
+  let countdown = n;
+  while (countdown > 0) {
+    console.log (--countdown);
+  }
+  return "done";
+}
+
+function doWhileLoop(i) {
+  var n = 0;
+  function incrementVariable() {
+  n = n + 1;
+  return n;
+}
+do {
+console.log("I run once regardless");
+} while (incrementVariable() < i);
+} 
