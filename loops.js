0561baa915a8534d849b31c779ed19687e31e61e
Finn Thye <piqueen@gmail.com>
2019-04-12 02:20:55 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array) {
+  for (var i = 0; i < 26; i++) {
+    `"I am ${i} strange loop${i === 0 ? '' : 's'}."`;
+  // if (i === 0){
+  //   array.push('I am 1 strange loop.')
+  // } else {
+  //   array.push(`"I am ${i} strange loops."`);
+  //   }
+  }
+  return array;
+}
