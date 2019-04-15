772e703efd2ad88b21269accf0d5ad23324d96da
Finn Thye <piqueen@gmail.com>
2019-04-15 16:53:40 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,32 @@
 function forLoop(array) {
-  for (var i = 0; i < 26; i++) {
-    `"I am ${i} strange loop${i === 0 ? '' : 's'}."`;
-  // if (i === 0){
-  //   array.push('I am 1 strange loop.')
-  // } else {
-  //   array.push(`"I am ${i} strange loops."`);
-  //   }
+  for (var i = 0; i < 25; i++) {
+    if (i === 1){
+      array.push('I am 1 strange loop.');
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
   }
   return array;
+}
+
+
+function whileLoop(number) {
+  while (number >0) {
+    console.log(--number);
+  }
+  return 'done';
+}
+
+
+function doWhileLoop(integer) {
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  
+  do {
+    console.log("I run once regardless");
+  } while (incrementVariable() < integer);
 }
