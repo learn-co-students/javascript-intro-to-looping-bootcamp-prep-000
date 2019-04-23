cf7154e4ad3043508ed688ff8628ebd455ecc4c6
Danielle Martinoli <daniechris5@gmail.com>
2019-04-23 17:32:46 +0000

    Done.

--- loops.js
+++ loops.js
@@ -11,6 +11,43 @@ function whileLoop(n > 0) {
   }
 }
 
+function forloop(array) {
+  for (let i = 0; i < array.length; i++)
+    {console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.");
+  }
+}
+
+let countdown = n
+function whileLoop(n > 0) {
+  while (countdown > 0) {
+    console.log("Done");
+  }
+}
+
+function forloop(array) {
+  for (let i = 0; i < array.length; i++)
+    {console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.");
+  }
+}
+
+let countdown = n
+function whileLoop(n > 0) {
+  while (countdown > 0) {
+    console.log("Done");
+  }
+}
+
+var i= 0;
+function doWhileLoop(num) {
+  i = i + 1;
+  return i;
+}
+  do {
+    console.log("I run once regardless.")
+  }
+  while (incrementVariable() < 1);
+}
+
 var i= 0;
 function doWhileLoop(num) {
   i = i + 1;
