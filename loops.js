ec13bd1b73e84b644bdb7cf2b0251e81c60ee175
Danielle Martinoli <daniechris5@gmail.com>
2019-04-23 17:38:16 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,60 +1,35 @@
-function forloop(array) {
+function forloop(array); {
   for (let i = 0; i < array.length; i++)
     {console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.");
   }
 }
 
 let countdown = n
-function whileLoop(n > 0) {
-  while (countdown > 0) {
-    console.log("Done");
-  }
-}
-
-function forloop(array) {
-  for (let i = 0; i < array.length; i++)
-    {console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.");
-  }
-}
-
-let countdown = n
-function whileLoop(n > 0) {
-  while (countdown > 0) {
-    console.log("Done");
-  }
-}
-
-function forloop(array) {
-  for (let i = 0; i < array.length; i++)
-    {console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.");
-  }
-}
-
-let countdown = n
-function whileLoop(n > 0) {
+function whileLoop(n > 0); {
   while (countdown > 0) {
     console.log("Done");
   }
 }
 
 var i= 0;
-function doWhileLoop(num) {
+function doWhileLoop(num); {
   i = i + 1;
   return i;
 }
   do {
-    console.log("I run once regardless.")
+    console.log("I run once regardless.");
   }
   while (incrementVariable() < 1);
 }
 
+
 var i= 0;
-function doWhileLoop(num) {
+function doWhileLoop(num); {
   i = i + 1;
   return i;
 }
   do {
-    console.log("I run once regardless.")
+    console.log("I run once regardless.");
   }
   while (incrementVariable() < 10);
 }
