1c4b8b77d48982485ac60e90e0a53dc371b7ea74
Danielle Martinoli <daniechris5@gmail.com>
2019-04-23 16:58:18 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,13 +1,13 @@
-function forloop (array) {
-  let (i = 0; i < 25; i++)
+function forloop(array) {
+  let (i = 0; i < array.length; i++)
     console.log("I am " + i + " strange loop");
 }
 
 let countdown = 100
-whileLoop (countdown > 0) {
+whileLoop(countdown > 0) {
   console.log(--countdown);
 }
 
 function doWhileLoop(num) {
-
+  console.log("I run once regardless.")
 }
