8bb438fa406381b50fb4c97680334c3cec11a2af
Danielle Martinoli <daniechris5@gmail.com>
2019-04-23 16:33:32 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forloop (array) {
-    for (let i = 0; i < 25; i++) {
-        console.log("I am " + i + " strange loop");
+  let (i = 0; i < 25; i++)
+    console.log("I am " + i + " strange loop");
 }
 
 let countdown = 100
