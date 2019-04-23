988a2b0d09f17da5269586234501ffe7e311941b
Danielle Martinoli <daniechris5@gmail.com>
2019-04-23 22:42:09 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i <25; i++) {
-    array.push(`I am ${i} strange loop${i === 0 ? '.' : 's.'}`);
+    array.push(`I am ${i} strange loops`);
   }
   return array
 }
