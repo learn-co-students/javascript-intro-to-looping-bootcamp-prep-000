f3ba7e633bab6d9842d17b1f3c8b4d590fbeb1c6
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-26 14:13:08 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,31 +1,15 @@
-// var array 
-
-// function forLoop(array) {
-// for (let i = 0; i < 25; i++) {
-//   console.log(`"I am + ${i} strange loop ${i=== 0 ? '' : 's'}."`)
-//   }
-// }
-
-// function forLoop(array) {
-// //Somemhow I need to link an array to this function
-
-// // The code below prints out - by itself 25 times 
-//   for ( i = 0; i < array.length || i <= 25; i++) {
-//   console.log(`"I am  ${i} strange loop${i=== 0 ? '' : 's'}."`)
-//   }
-//   let counter = i + 1
-
- 
-// }
-let countdown = n 
-
-function whileLoop(n) {
-  let countdown = 10;
-  while (countdown > n){
-  countdown = countdown -= 1;
-console.log(`done`);
-break;
+function forLoop(array){
+for(let i = 0; array.length < 25; i++){
+//   console.log(array);
+  if (array.length === 1){
+    array.unshift("I am 1 strange loop");
+   }else{
+     array.push(`I am ${i} strange loops.`);
+   }
+  }
+return array
 }
 
-}
+
+
 
