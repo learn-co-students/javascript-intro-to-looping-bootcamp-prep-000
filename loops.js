baec3f7421aaa665b4eaa1a4da81cd66e10cf9ac
Patrick Candlin <pctennislessons@gmail.com>
2019-04-27 20:07:43 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop (array) {
+  x = "";
+for (let i = 0; i < 25; i++) {
+if (i === 1) { 
+   	x = "I am " + parseInt(i) +" strange loop.";
+ array.push(x);
+      } else{x= "I am 24 strange loops.";
+ array.push(x);
+        }
+        
+  }
+  return array.slice();
+} 
