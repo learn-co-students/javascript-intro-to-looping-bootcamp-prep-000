265c234ac8e6b594afd117281674ee78d443194d
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-29 00:58:07 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 //loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."` to an array 25 times:
 function forLoop(array){
-for (let i=1; i<=25; i++){
-  if (i===1){
-    array.push("I am " + ${i} + " strange loop.");
+for (let i=0; i<25; i++){
+  if (i===0){
+    array.push(`I am ${i} strange loop.`);
   } else {
-    array.push("I am " + ${i} + " strange loops.");
+    array.push(`I am ${i} strange loops.`);
   }
 }
   return array;
