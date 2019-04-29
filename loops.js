73822321b50527d6fabc13d4cb243c26e3875dcc
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-29 02:21:41 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,10 +2,32 @@
 function forLoop(array){
 for (let i=0; i<25; i++){
   if (i===0){
-    array.push(`I am ${i} strange loop.`);
+    array.push("I am 1 strange loop.");
   } else {
     array.push(`I am ${i} strange loops.`);
   }
 }
   return array;
+}
+
+function whileLoop(n){
+  while (n>0){
+    console.log(n);
+    n=n-1;
+  }
+  return "done"
+}
+
+let i=0;
+function incrementVariable() {
+i = i + 1;
+return i;
+}
+
+function doWhileLoop(num){
+do {
+console.log("I run once regardless.");
+} while (incrementVariable() < num){
+console.log("I run once regardless.")
+}
 }
