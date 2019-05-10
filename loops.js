37597482c6973c51f9a556cb199e0b98758c0374
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-05-10 16:37:19 +0000

    Done.

--- loops.js
+++ loops.js
@@ -11,42 +11,48 @@
 // return array
 //}
 
-let array = []
+//let array = []
 function forLoop(array){
   for(let i = 0; i < 25; i += 1 ){
 
- array.push(`${i}`);
-if ( array[i] == 1){
-  console.log('I am 1 strange loop.');
-  }else if(array[i] > 1)
- console.log(`"I am ${i} strange loops."`);
+// array.push(`${i}`);
+if ( i == 1){
+  array.push('I am 1 strange loop.');
+  }  else
+ array.push(`I am ${i} strange loops.`);
  
-}
+ }
 return array
 }
 //forLoop(array);
 
-// //works dont edit - while looop
-// function whileLoop(n){
-// while ( 0 < n ){
-//   console.log(--n);
-// }
-// if (n <= 1 ){
-// return 'done';
-//   }
-// }
+//works dont edit - while looop
+function whileLoop(n){
+while ( 0 < n ){
+  console.log(--n);
+}
+if (n <= 1 ){
+return 'done';
+  }
+}
 
  //while do Loop
 
 function doWhileLoop(num) {
- var i = 0;
+  var i = 0;
+ function incrementVariable() {
+   
   i = i + 1;
   return i;
-}
+   }
 
 do{
   console.log("I run once regardless. ");
 
   
-}while (doWhileLoop() < num);
+}while (incrementVariable() < num);
+  
+}
+
+
 
