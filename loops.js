b8ffcc0ba867b3fba7b37a1b67174589d2a8b8d9
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-25 19:12:07 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,8 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++)
+    if (i === 1) {
+    console.log(`"I am ${i} strange loop."`);
+  } else {
+  console.log(`"I am ${i} strange loops."`);
+  } return array;
+}
