0e3ed622ed8995330e5b32111cec42552f1eff91
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-26 22:44:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -8,3 +8,24 @@ function forLoop(array) {
 }
 
 
+function whileLoop(n) {
+  while (n > 0) {
+  console.log(--n);
+  }
+  return ('done');
+}
+
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(num) {
+  
+  do {
+  console.log("I run once regardless.");
+} while (incrementVariable() <= num);
+}
