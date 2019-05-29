f760259695a9e9f6cb0839ffc4528f334ef78074
Vik Vangala <vvang044@gmail.com>
2019-05-29 17:22:50 +0000

    Done.

--- loops.js
+++ loops.js
@@ -8,3 +8,12 @@ function forLoop(array) {
   }
   return array;
 }
+
+function whileLoop(n) {
+  let countdown = n;
+
+     while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return "done";
+  }
