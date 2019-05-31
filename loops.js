7c307ba6f3da4f31d7aeb5b5b19ae5fa2fa31b7b
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-31 22:09:12 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  array=[]
+  for (let i=0; i<25; i++) {
+    if (i===1) {
+      array.push(`I am 1 strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while(n>0) {
+    console.log(n--)
+  }
+  return 'done'
+}
+
+function doWhileLoop(num) {
+  function decrementVariable() {
+    num--
+    return num
+  }
+  do {
+    console.log('I run once regardless.')
+  } while(decrementVariable()>0)
+}
