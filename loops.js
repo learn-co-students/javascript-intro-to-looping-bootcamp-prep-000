8f60aa6c41b34c3b5cf18c9a74215694dfe03e7c
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-05 17:04:14 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array){
   for (let i = 0; i < 25; i++){
     if (i === 1){
       newArray.push("I am 1 strange loop.");
-    } else { newArray.push("I am ${i} strange loops.")}
+    } else { newArray.push(`I am ${i} strange loops.`)}
 }
   return newArray
 } 
