69bafee58dfd9741a66a0f92b8f5f38652b4e914
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-06 14:32:49 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,25 @@
 function forLoop(array){
-  var newArray = []
   for (let i = 0; i < 25; i++){
     if (i === 1){
-      newArray.push("I am 1 strange loop.");
-    } else { newArray.push(`I am ${i} strange loops.`)}
+      array.push("I am 1 strange loop.");
+    } else { array.push(`I am ${i} strange loops.`)}
 }
-  return newArray
+  return array
 } 
+function whileLoop(num){
+  let countdown = 20;
+  while (countdown > num){
+    console.log(--countdown)
+  }
+  return 'done'
+}
+function doWhileLoop(int){
+  function incrementVariable() {
+  int = int + 10;
+  return int;
+}
+do {
+  console.log("I run once regardless.")
+} while (incrementVariable() < int)
+return int
+}
