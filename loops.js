4ff140bcc68977cd5c0f8dd341d1df069b3be816
Carlos Didier Fernandez <didierfdz@gmail.com>
2019-06-07 19:56:28 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,38 @@
+function forLoop(array) {
+  
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am 1 strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  
+  return array;
+}
+
+function whileLoop(n) {
+  var countdown = n;
+  
+  while (countdown > 0) {
+    console.log(countdown);
+    countdown -= 1;
+    // can do console.log(--countdown);
+  }
+  
+  return 'done';
+}
+
+
+var i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  
+function doWhileLoop(num) {
+  do {
+    console.log('I run once regardless');
+  } while
+  (incrementVariable() <= num);
+}
