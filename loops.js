9347b076e58ba4855d028435ede15486a3f4570b
droll-accelerator-1483 <srsani@gmail.com>
2019-06-21 14:39:54 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,39 @@
+function forLoop(array){
+  for (let i = 0; i <25; i ++){
+    if (i ===1){
+      
+      array.push("I am 1 strange loop.")
+    }
+    if (i ===0){
+      
+      array.push("I am strange loop.")
+    }
+    if (i>1){array.push(`I am ${i} strange loops.`)}
+    
+  }
+  return array
+}
+
+function whileLoop(number){
+ while (number > 0) {
+  
+  console.log(--number);}
+  if (number ===0){
+    return "done"
+  }
+  
+}
+
+
+function doWhileLoop(num){
+  var i = 0
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+  do{
+    
+    console.log("I run once regardless.")
+   } while (incrementVariable() < num)
+}
