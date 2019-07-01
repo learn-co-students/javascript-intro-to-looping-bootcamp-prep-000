3dd8521ba56ea9c103bfa52709ad2004f4743554
Adam Brown <adam.brown.betty@gmail.com>
2019-07-01 01:46:12 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,27 +1,41 @@
+/*  use the FOR loop, make a special use case for the number 1 by using IF / ELSE control flow
+use PUSH method to add each new string
+
+*/
+
 function forLoop(array) {
 
 for (let i = 0; i < 25; i++) {
    
    if (i === 1) {
      
-     array.push('I am 1 strange loop.') 
+     array.push('I am 1 strange loop.'); 
    } else {
-      array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`);
    } 
   }
   
-    return array
+    return array;
 }
 
+
+/*  so whatever n starts as, it will log the number and subtract 1 from it
+add special case when it gets to 0.
+*/
 function whileLoop(n) {
   while (n > 0) {
-    console.log(--n);
+    console.log(n--);
   }
   
   if (n === 0) {
-    return 'done'
+    return 'done';
   }
 }
+
+/*  tell the DO what to do  first, 
+then the WHILE the condition is still true
+(value returned from  incrementVariable function is more than the number given)
+*/
 
 function doWhileLoop(num) {
   var i = 0;
