3a1f5a96844068eb1681b6c2a8407da9d27af776
Folasayo Akins <fola4ril@gmail.com>
2019-07-08 13:20:59 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am 1 strange loop.`);
+    }
+    else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return "done";
+}
+
+function doWhileLoop(num){
+  function maybeTrue() {
+    num--
+    return num
+}
+do{
+  console.log('I run once regardless.')
+}
+while (num > 0 && maybeTrue()){
+  return num
+  }
+}
+ doWhileLoop(10)
