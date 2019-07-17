45cdce79b0a83b6371ad7ad9a19fe4ad2d4a7824
Aaron Blum <blumaa@gmail.com>
2019-07-17 15:41:24 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop() {
-  for (var i = 0; i < 26; i++) {
+  for (var i = 0; i < 25; i++) {
     if (i===1) {
-      console.log("I am a strange loop.")
+      console.log("I am 1 strange loop.")
     } else {
-      console.log("I am ${i} time");
+      console.log("I am " + i + "time");
     }
   }
 }
