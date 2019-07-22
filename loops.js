856cf8c2329e31ec228afbb821b58f016ed46754
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-22 06:37:39 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++)
+  {
+    if (i === 1) {
+    array.push( `I am 1 strange loop.`);
+    }
+    else {
+    array.push( `I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(n){
+  var countdown = n;
+  while (countdown > 0) {
+  console.log(--countdown);
+}
+return 'done';
+}
+
+function incrementVariable(i) {
+  i = i + 1;
+  return i;
+}
+do {
+    console.log("I run once regardless");
+} while (incrementVariable() < 5);
