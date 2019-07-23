497aca0ed2208dd2f84b15a626a9b538af23cfce
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-23 23:27:42 +0000

    Done.

--- loops.js
+++ loops.js
@@ -15,10 +15,12 @@ _interpolate_ `i`?) Then return the array.
 }
   /*Create a function called whileLoop in loops.js. The function should take a number as an argument. Using a while loop, count down (using console.log) from the passed in number to 0. Then return the string 'done'.
 */
+function whileloop() {
 let countdown = 100;
 while (countdown > 0) {
-  console.log(counterTwo);
-  counterTwo++;
+  console.log(countdown);
+  countdown++;
+}
 }
 
 /*Define a function called doWhileLoop in loops.js. The function should take an integer as an argument. Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless." while incrementVariable() returns a number less than the parameter received. (Your condition might look something like incrementVariable() < num.) Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked.*/
