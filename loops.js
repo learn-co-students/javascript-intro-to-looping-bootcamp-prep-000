2a6006baad130ed119784082256c6af4f2c7d5d8
Gian Moran <gpmoran@uh.edu>
2019-07-28 16:13:59 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(arr){
+  for(var i = 0; i < 25;i++){
+    if( i === 1){
+      arr.push(`I am 1 strange loop.`)
+    }
+    else
+    arr.push(`I am ${i} strange loops.`)
+  }
+  return arr
+}
+function whileLoop(num){
+  var countdown = num
+  while(countdown > 0){
+    console.log(--countdown)
+    
+  }
+  return 'done'
+}
+function doWhileLoop(num){
+  var i = 0
+  function incrementVariable(){
+    i = i++
+    return i
+  }
+  do{
+    console.log("I run once regardless")
+  }while(incrementVariable() < 10)
+  
+}
