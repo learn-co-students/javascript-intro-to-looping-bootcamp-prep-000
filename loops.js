e8f3f4270dd38ea2c70e231e50ce7301d72ad30e
Jon Caneiro <joncaneiro@gmail.com>
2019-07-30 23:58:13 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  if (i === 1) {
+    console.log("I am 1 strange loop")  
+ } else {
+   array.push("I am" + " " + i + " " + "strange loop".)
+ }
+  return array
+}
+}
+
