95ebd4ac916360c3b574ba2faea19d82da654a86
Niki Nedelchev <nedelchev.niki@gmail.com>
2019-08-03 18:31:08 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,40 @@
+function forLoop(array) {
+
+  for (let i = 0; i < 25; i++) {
+
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+
+  };
+
+  return array;
+
+}
+
+function whileLoop(number) {
+
+  while (number > 0) {
+
+    console.log(number--);
+
+  }
+
+  return "done";
+
+}
+
+function doWhileLoop(num) {
+
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+
+
+  do {
+    console.log("I run once regardless");
+  } while (incrementVariable() < num);
+
+}
