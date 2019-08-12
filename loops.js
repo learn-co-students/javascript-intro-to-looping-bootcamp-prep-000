9aed90dc1b9ab1dd24e5c087c436ef349d1918c6
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-08-12 22:36:48 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,4 +1,21 @@
-function forLoop() {
-for (var i=0; i < 25; i++) { if(i = 1){ return "I am 1 strange loop"}
-  else {return 'I am ${i} strange loops'}
-}}
+function forLoop(array) {
+for (let i=0; i < 25; i++) { if(i === 1){ array.push("I am 1 strange loop.")}
+  else {array.push(`I am ${i} strange loops.`)}
+}
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {console.log(--n)}
+  return 'done';
+
+}
+
+var i = -1;
+function incrementVariable(){
+  i = i + 1;
+  return i;
+}
+function doWhileLoop(int){ do {console.log('I run once regardless.');}
+while (incrementVariable() < int );
+}
