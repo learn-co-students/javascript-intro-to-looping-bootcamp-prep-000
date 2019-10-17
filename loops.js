b68eb049c8b08068361a94f9c4f751ea11c473bd
nsabir <awesomeartist15@gmail.com>
2019-10-17 03:59:26 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,47 @@
+var i = 0
+
+function forLoop(array) {
+  for (i = 0; i < 25; i++){
+    if (i === 1){
+      array.push("I am " + i + " strange loop.")
+    }
+    else {
+      array.push("I am " + i + " strange loops.")
+    }
+   }
+  return array
+}
+
+function whileLoop(n){
+  while (n > 0){
+    if (n > 0){
+      console.log(--n)
+    }
+    else {
+      return 'done'
+    }
+  }
+  return 'done'
+}
+
+function doWhileLoop(num){
+  while (num > 0){
+    if (num > 0){
+      console.log(--num)
+    }
+    else {
+      return 'done'
+    }
+  }
+  return 'done'
+}
+
+var num = 0;
+ 
+function doWhileLoop(num) {
+  num = num + 1;
+  do {
+   console.log("I run once regardless.");
+   } while (incrementVariable() < num);
+}
+ 
