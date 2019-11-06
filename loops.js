299f84338f6bc0ebcea44642e8f6d3161540a91c
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-06 02:55:00 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,45 @@
-for (let i = 0; i < 25; i++){
-  if (i === 1)
-  console.log("I am 1 strange loop.");
+function forLoop(array){
+ 
+  for (let i = 0; i < 25; i++){
+  if (i===1)
+  {
+  array.push("I am 1 strange loop.");
+    
+  }
   
   else{
-    console.log(`"I am ${i} strange loops."`);
+   array.push(`I am ${i} strange loops.`);
   }
 }
+return array;
+}
 
+function whileLoop(n){
+  let countdown = n;
+ 
+while (countdown > 0) {
+  console.log (--countdown);
+}if (countdown === 0){
+  return 'done';
+}
+
+}
+
+var i = 0;
+ 
+function doWhileLoop(num) {
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+  
+}
+  
 
 
