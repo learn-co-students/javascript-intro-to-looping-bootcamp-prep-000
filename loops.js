d721ca8e5edf4caa8f4a890652dc2250593dbddf
Ian McKenzie <imckenzie777@gmail.com>
2020-02-27 14:42:01 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop (a = []) {
+
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      a.push(`I am ${i} strange loop.`);
+    } else {
+      a.push(`I am ${i} strange loops.`);
+    }
+  }
+  return a;
+}
+console.log(forLoop());
+
+function whileLoop(n) {
+  let countdown = n;
+
+    while (countdown > 0) {
+      console.log(--countdown);
+    }
+  return "done";
+}
+console.log(whileLoop());
+
+function doWhileLoop(num) {
+	let i = 0;
+
+	do {
+		function incrementVariable() {
+			i = i + 1;
+			return i;
+
+		}
+		console.log(`I run once regardless.`);
+	} while (incrementVariable() < num);
+}
+doWhileLoop();
