0e019e12909f8ea2c5f790624844f0c7a4f44354
Francisco Andaur <francisco.andaur@icloud.com>
2020-02-28 13:47:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array) { // Build forLoop function takes array as argument
+	for (let i = 0; i < 25; i++) { //using for loop add string to the array 25 times.
+		if (i === 1) { // if i value is 1
+			array.push('I am 1 strange loop.'); //add the string "I am 1 strange loop.""
+		} else { //if i is anything else
+			array.push(`I am ${i} strange loops.`); //add this line <-----
+		}
+	}
+
+	return array;
+}
+
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n);
+	}
+
+	return 'done';
+}
+
+function doWhileLoop(num) {
+	var i = 0;
+
+	function addOne() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (addOne() < num);
+}
