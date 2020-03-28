7c7047d10344fee952b25856ccb2822896cf013a
zaid85j <zaid85j@gmail.com>
2020-03-28 19:59:00 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
-var array=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+var array=[]
 function forLoop(array){
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < 25; i++) {
     if (array[i]==1){
       array.push(`I am ${i} strange loop.`)
     }else{
