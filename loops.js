781d1011526545dbd0b1fb0da7cbb47764a76395
zaid85j <zaid85j@gmail.com>
2020-03-28 19:42:01 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
 function forLoop(){
-  
+  for (let i = 0; i < 25; i++) {
+    if (i==1){
+       console.log(`I am 1 strange loops.`)
+    }else{
+ console.log(`I am ${i} strange loops.`)}
+    }
 }
