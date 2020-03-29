c78625684d4b5114fa06739b5a316275cfee01ca
zaid85j <zaid85j@gmail.com>
2020-03-29 08:35:09 +0000

    Done.

--- loops.js
+++ loops.js
@@ -18,6 +18,15 @@ function whileLoop(num){
   return 'done'
 }
  function doWhileLoop(){
-   
+   let i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
  }
- }
+ 
