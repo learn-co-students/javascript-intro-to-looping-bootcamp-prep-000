3d86a0460f36cec20f0c2e8af69b2f0e4647c60e
casey bradford <casey@flatironschool.com>
2020-04-07 22:05:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array) {
+for (let i = 0; i < 25; i++) {
+  console.log(i);
+  array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+}
+  return array
+}
+
+function whileLoop(n){
+  while (n > 0){
+    n--;
+    console.log(n);
+  }
+  return 'done'
+}
+
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+
+function doWhileLoop(num){
+  do{
+    console.log("I run once regardless.");
+  } while (incrementVariable()  <= num);
+}
+
