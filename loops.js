2a6dff3595fd0c95e19419fa15b67e1cda06ada9
Mike Gretton <mikegretton@gmail.com>
2020-05-05 23:12:10 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+  };
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  };
+  return 'done';
+}
+
+function doWhileLoop(n) {
+  do {
+    console.log("I run once regardless.");
+    n--;
+  } while (n > 0);
+}
