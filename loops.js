96fdd64fb0cfa63120629d53a5731ae34f6d8cd6
Austin Buhler <austinbuhler@gmail.com>
2020-05-08 16:28:47 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for (let i = 0, i < array.length; i++) {
+    if (i = 0) {
+      console.log(`I am ${i} strange loop.`)
+    }
+    else {
+      console.log(`I am ${i} strange loops.`)
+    }
+  }
+}
