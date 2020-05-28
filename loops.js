da81abb9f76d8746bc49afe640534a82a0f21195
Mackenzie Bruns <mackenziebruns503@gmail.com>
2020-05-28 20:21:57 +0000

    Done.

--- loops.js
+++ loops.js
@@ -30,12 +30,13 @@ function whileLoop(n){
      }
   return ("done")
 }
-
 let i = 0;
+ 
 function incrementVariable() {
   i = i + 1;
   return i;
 }
+
 function doWhileLoop(num){
   do { console.log("I run once regardless.")}
   while (incrementVariable()<= num);
