cedb280ac75d1aff00d1d68e00fa013022ddfe62
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-07-01 05:40:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,31 @@
 function forLoop(array) {
-  for (let i = 0; i < 26; i++ ) 
-    if( i === 0) { 
-      array.push(`" I am 1 starnge wood"`)
-    } else {
-      array.push(`" I am  ${i} starnghe loops."`)
-    }
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+
+	return array;
 }
 
-return array;
+function whileLoop(num) {
+	while (num > 0) {
+		console.log(--num);
+	}
+
+	return 'done';
+}
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+}
