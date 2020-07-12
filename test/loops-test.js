2e4d1c38e13e19303dadfe8dda1478294ebbf35f
Myles <mylesssalazar@gmail.com>
2020-07-12 16:59:32 +0000

    Done.

--- test/loops-test.js
+++ test/loops-test.js
@@ -62,8 +62,8 @@ describe('loops', () => {
 
     it ('console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.', () => {
       const spy = chai.spy.on(console, 'log');
-      doWhileLoop(10);
-      expect(spy).to.have.been.called.exactly(10);
+      doWhileLoop(20);
+      expect(spy).to.have.been.called.exactly(20);
     })
   })
 })
