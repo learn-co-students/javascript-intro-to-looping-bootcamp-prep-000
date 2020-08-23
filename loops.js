1b69a0f97c9aee7144eeaa2685c5ee343df811bb
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-23 04:31:54 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop (array) {
+  for (let i=0;i<25; i++){ 
+    if (i ===1) {array.push("I am 1 strange loop.")   }
+  else {array.push(`I am ${i} strange loops.`)}
+  }
+  return array
+}
+
+function whileLoop(number){
+  while (number>0){
+    console.log(number)
+    number-=1
+  }
+  return "done"
+}
+
+function doWhileLoop(num){
+    let i=0
+    do {console.log("I run once regardless.")}
+    while ((i+=1) <num) 
+      } 
