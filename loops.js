f6862dc3ca6f1b97ab46092264f197bc0b65a27c
Chemar Wright <chemarrian@gmail.com>
2020-09-18 01:11:07 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,6 @@
+function forLoop(array){
+  for (let i = 0; i<25; i++)
+  if (i=1) {console.log ("I am 1 strange loop")
+} else  { console.log (" I am ${i} strange loop")}
+return `I am ${i} strange loops`
+}
